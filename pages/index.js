9aad336dfe48baf6d05f5e47135d868b36f09d84
Joseph Dubon <josephdubon@pm.me>
2022-03-02 16:09:50 -0800

    added bootstrap components for basic structure

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,13 @@
 const Index = () => {
     return (<>
-            <h1>Americoders Advanced Learning Center</h1>
-            <p>Welcome!</p>
-        </>)
+        <div className="jumbotron jumbotron-fluid">
+            <div className="container">
+                <h1 className="display-4">Americoders - Center for Advanced Learners</h1>
+                <p className="lead">This is a modified jumbotron that occupies the entire horizontal space of its
+                    parent.</p>
+            </div>
+        </div>
+    </>)
 }
 
 export default Index
