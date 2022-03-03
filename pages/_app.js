955b0f1e96db31dc86a542a2231ee43e14c867f1
Joseph Dubon <josephdubon@pm.me>
2022-03-02 16:09:28 -0800

    added global css and useEffect for bootstrap js components

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,16 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'antd/dist/antd.css'
+import '../public/css/styles.css'
+import {useEffect} from "react";
 
 function MyApp({Component, pageProps}) {
+
+    // add this for bootstrap js components to render correctly
+    useEffect(() => {
+        import('bootstrap/dist/js/bootstrap');
+    }, []);
+
+
     // get base component and any props
     return <Component {...pageProps}/>
 }
