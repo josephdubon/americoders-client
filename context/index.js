d250f771ffe6ca7ca55758f2a55886b965678ec4
Joseph Dubon <josephdubon@pm.me>
2022-03-10 13:59:42 -0800

    created basic config for a redux style state management system, added initial state, context, a reducer, and a provider

---
+++ context/index.js
@@ -0,0 +1,36 @@
+import {createContext, useReducer} from 'react'
+import {useRefreshRoot} from "next/dist/client/streaming/refresh";
+
+// initialize state
+const initialState = {
+    user: null,
+}
+
+// create context
+const Context = createContext()
+
+// create reducer function
+// anytime you dispatch an action it returns the type and payload
+const rootReducer = (state, action) => {
+    switch (action.type) {
+        case 'LOGIN':
+            return {...state, user: action.payload} // return user payload
+        case 'LOGOUT':
+            return {...state, user: null} // return null user
+        default:
+            return state
+    }
+}
+
+// context provider, use provider to wrap app so state is available
+const Provider = ({children}) => {
+    const [state, dispatch] = useReducer(rootReducer, initialState)
+
+    return (
+        <Context.Provider value={{state, dispatch}}>
+            {children}
+        </Context.Provider>
+    )
+}
+
+export {Context, Provider}
