1cdd29bf9d383befcefc7d41fae29a5f899546d1
Joseph Dubon <josephdubon@pm.me>
2022-03-09 16:37:11 -0800

    added useState to set current page url path, added isServer const as workaround for process.browser being depreciated,  added selectedKeys to Menu component for array of keys from current paths,

--- components/TopNav.js
+++ components/TopNav.js
@@ -1,12 +1,26 @@
 import {Menu} from 'antd'
 import Link from 'next/Link'
 import {AppstoreOutlined, LoginOutlined, UserAddOutlined} from '@ant-design/icons'
+import {useEffect, useState} from 'react'
 
 
 // de-structure item from menu
 const {Item} = Menu
 
 const TopNav = () => {
+    // set state for current page/link
+    const [currentPage, setCurrentPage] = useState('')
+
+    const isServer = () => typeof window !== 'undefined'
+
+    // update state on page change
+    useEffect(() => {
+        // process.browser is depreciated use typeof instead
+        isServer() && setCurrentPage(window.location.pathname)
+        console.log(isServer(), window.location.pathname)
+    }, [isServer() && window.location.pathname])
+
+
     return (<>
         <Menu mode='horizontal'>
             <Item icon={<AppstoreOutlined/>}>
