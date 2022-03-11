835a979aa6f8e1f38d48e17278bc8b1e5c8ed6ca
Joseph Dubon <josephdubon@pm.me>
2022-03-11 10:00:00 -0800

    updated imports with useContext and useEffect

--- pages/register.js
+++ pages/register.js
@@ -1,4 +1,4 @@
-import {useState} from 'react'
+import {useContext, useEffect, useState} from 'react'
 import {SyncOutlined} from "@ant-design/icons";
 import Link from 'next/link'
 import {useRouter} from 'next/router'
