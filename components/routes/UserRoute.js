e0983113d872ab51b2b70ca7d9bce439246cb4d4
Joseph Dubon <josephdubon@pm.me>
2022-03-16 14:04:25 -0700

    Created new UserRoute component to wrap and secure selected pages from logged-out users just need to wrap return content in 'UserRoute'

---
+++ components/routes/UserRoute.js
@@ -0,0 +1,41 @@
+import {useEffect, useState} from 'react'
+import axios from 'axios'
+import {useRouter} from 'next/router'
+import {SyncOutlined} from '@ant-design/icons'
+
+const UserRoute = ({children}) => {
+    // state
+    const [ok, setOk] = useState(false)
+
+    // router
+    const router = useRouter()
+
+    useEffect(() => {
+        fetchUser()
+    }, [])
+
+    const fetchUser = async () => {
+        // try to get user match
+        try {
+            const {data} = await axios.get('/api/current-user')
+            if (data.ok) setOk(true) // if match found, set ok to true
+        } catch (err) {
+            console.log(err)
+            setOk(false) // if no match found, set ok to false
+            await router.push('/login') // redirect to login page
+        }
+    }
+
+    return (<>
+        {!ok ? (
+            // if ok is false, show loading screen
+            <SyncOutlined spin className='d-flex justify-content-center display-1 text-primary p-5'
+            />
+        ) : (
+            // if ok is true, show child element
+            <>{children}</>
+        )}
+    </>)
+}
+
+export default UserRoute
