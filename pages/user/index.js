70166900365999f64395429c1617b589095d5c1c
Joseph Dubon <josephdubon@pm.me>
2022-03-24 18:58:19 -0700

    removed footer

--- pages/user/index.js
+++ pages/user/index.js
@@ -34,17 +34,6 @@ const UserIndex = () => {
                 </div>
             </div>
         </main>
-
-        <footer className='text-muted py-5'>
-            <div className='container'>
-                <p className='float-end mb-1'>
-                    <a href='#'>Back to top</a>
-                </p>
-                <p className='mb-1'>Americoders Center for Advanced Learning</p>
-                <p className='mb-0'>Want to sign-up? <a href='/'>Visit the homepage</a> or read our <a
-                    href='#'>getting started guide</a>.</p>
-            </div>
-        </footer>
     </UserRoute>)
 }
 
