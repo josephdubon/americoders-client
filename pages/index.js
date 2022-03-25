52d1f20d73367796152834b8ab68d555445cf48e
Joseph Dubon <josephdubon@pm.me>
2022-03-24 18:58:45 -0700

    removed extra line

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,6 @@
 const Index = () => {
     return (<>
         <main>
-
             <section className="py-5 text-center container">
                 <div className="row py-lg-5">
                     <div className="col-lg-6 col-md-8 mx-auto">
