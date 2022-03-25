132113233348db88147b60a7059cf5794eed7475
Joseph Dubon <josephdubon@pm.me>
2022-03-25 14:36:25 -0700

    created new page for stripe/callback -the page user gets redirected to adter stripe onboarding

---
+++ pages/stripe/callback.js
@@ -0,0 +1,27 @@
+import {useContext, useEffect} from 'react'
+import {Context} from '../../context'
+import {SyncOutlined} from '@ant-design/icons'
+import axios from 'axios'
+
+const StripeCallback = () => {
+    // get user
+    const {state: {user}} = useContext(Context)
+
+    // update user info in db
+    useEffect(() => {
+        if (user) {
+            axios.post('/api/get-account-status')
+                .then(res => {
+                    console.log(res)
+                    // take user back to instructor page
+                    // window.location.href = '/instructor'
+                })
+        }
+    }, [user])
+
+    return (<>
+        <SyncOutlined spin className='d-flex justify-content-center display-1 text-danger p-5'/>
+    </>)
+}
+
+export default StripeCallback
