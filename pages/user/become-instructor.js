c234e0efbdde3eafc3145ce719e64a93493b88ee
Joseph Dubon <josephdubon@pm.me>
2022-03-25 07:44:54 -0700

    removed double quotes

--- pages/user/become-instructor.js
+++ pages/user/become-instructor.js
@@ -12,9 +12,9 @@ const BecomeInstructor = () => {
                 </div>
             </section>
 
-            <div className="album py-5 bg-light">
-                <div className="container">
-                    <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+            <div className='album py-5 bg-light'>
+                <div className='container'>
+                    <div className='row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3'>
                         <p className='lead'>
                             Become Instructor
                         </p>
