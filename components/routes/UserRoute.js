75d6a872108e5152b74496786424f9a382765c47
Joseph Dubon <josephdubon@pm.me>
2022-03-24 19:02:43 -0700

    imported UserNav and added to conditional render as sidebar

--- components/routes/UserRoute.js
+++ components/routes/UserRoute.js
@@ -2,6 +2,7 @@ import {useEffect, useState} from 'react'
 import axios from 'axios'
 import {useRouter} from 'next/router'
 import {SyncOutlined} from '@ant-design/icons'
+import UserNav from '../nav/UserNav'
 
 const UserRoute = ({children}) => {
     // state
@@ -33,7 +34,16 @@ const UserRoute = ({children}) => {
             />
         ) : (
             // if ok is true, show child element
-            <>{children}</>
+            <>
+                <div className='container-fluid'>
+                    <div className='row'>
+                        <div className='col-md-2'>
+                            <UserNav/>
+                        </div>
+                        <div className='col-md-10'>{children}</div>
+                    </div>
+                </div>
+            </>
         )}
     </>)
 }
