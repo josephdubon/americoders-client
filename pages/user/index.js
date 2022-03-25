4e6ab1fa98d0e9fe231ae41a1e0b77f11155f2ac
Joseph Dubon <josephdubon@pm.me>
2022-03-24 18:58:09 -0700

    renamed to user dashboard

--- pages/user/index.js
+++ pages/user/index.js
@@ -14,7 +14,7 @@ const UserIndex = () => {
             <section className='py-5 text-center container'>
                 <div className='row py-lg-5'>
                     <div className='col-lg-6 col-md-8 mx-auto'>
-                        <h1 className='fw-light'>User Page</h1>
+                        <h1 className='fw-light'>User Dashboard</h1>
                         <p className='lead text-muted'>Something short and leading about the collection
                             below—its
                             contents, the creator, etc. Make it short and sweet, but not too short so folks
