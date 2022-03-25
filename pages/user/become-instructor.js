41fe84fc5f4c21266ac748915796ce221d995f29
Joseph Dubon <josephdubon@pm.me>
2022-03-25 07:47:03 -0700

    added state, context, and new function becomeInstructor to test button and api with

--- pages/user/become-instructor.js
+++ pages/user/become-instructor.js
@@ -1,4 +1,50 @@
+import {useContext, useState} from 'react'
+import {Context} from '../../context'
+import {Button} from 'antd'
+import {LoadingOutlined, SettingOutlined, UserSwitchOutlined} from '@ant-design/icons'
+import axios from 'axios'
+import {toast} from 'react-toastify'
+
 const BecomeInstructor = () => {
+    // state
+    const [loading, setLoading] = useState(false)
+
+    // get user
+    const {
+        state: {user}
+    } = useContext(Context)
+
+    // become instructor logic
+    const becomeInstructor = () => {
+        // console.log('become instructor button')
+
+        setLoading(true)
+
+        // api config
+        axios
+            .post('/api/make-instructor')
+            .then((res) => {
+                console.log(res)
+                window.location.href = res.data // open on-boarding in new window
+            })
+            .catch((err) => {
+                console.log(err.response.status)
+
+                // notification config
+                toast('Stripe on-boarding failed. Try again.', {
+                    position: 'top-center',
+                    autoClose: 5000,
+                    hideProgressBar: false,
+                    closeOnClick: true,
+                    pauseOnHover: true,
+                    draggable: true,
+                    progress: undefined,
+                })
+
+                setLoading(false)
+            })
+    }
+
     return (<>
         <main>
             <section className='py-5 text-center container'>
