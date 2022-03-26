d53f7372009d0caffebf6406cd147405a3c5d911
Joseph Dubon <josephdubon@pm.me>
2022-03-26 09:27:13 -0700

    added state for form values, added form logic functions, added courseCreateForm with conditional rendering

--- pages/instructor/course/create.js
+++ pages/instructor/course/create.js
@@ -9,6 +9,118 @@ import InstructorRoute from '../../../components/routes/InstructorRoute'
 const {Option} = Select
 
 const CreateCourse = () => {
+    // state
+    const [values, setValues] = useState({
+        name: '',
+        description: '',
+        price: '9.99',
+        uploading: false,
+        paid: true,
+        loading: false,
+        imagePreview: '',
+    })
+
+    // form logic: values
+    const handleChange = (e) => {
+        setValues({...values, [e.target.name]: e.target.value})
+    }
+
+    // form logic: images
+    const handleImage = () => {
+        //
+    }
+
+    // form logic: submission
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        console.log('VALUES ', values)
+    }
+
+    // form logic: create form
+    const courseCreateForm = () => (
+        <form onSubmit={handleSubmit}>
+            <div className='form-group '>
+                {/* name */}
+                <input
+                    type='text'
+                    name='name'
+                    className='form-control'
+                    value={values.name}
+                    placeholder='Name'
+                    onChange={handleChange}
+                />
+            </div>
+
+            {/* description */}
+            <div className='form-group'>
+                <textarea
+                    name='description'
+                    id=''
+                    cols='7'
+                    rows='7'
+                    className='form-control'
+                    value={values.description}
+                    placeholder='Description'
+                    onChange={handleChange}
+                >
+                </textarea>
+            </div>
+
+            {/* paid or free course */}
+            <div className='form-row'>
+                <div className='col'>
+                    <div className='form-group d-grid'>
+                        <Select
+                            size='large'
+                            value={values.paid}
+                            onChange={(v) => setValues({...values, paid: !values.paid})}
+                        >
+                            <Option value={true}>Paid</Option>
+                            <Option value={false}>Free</Option>
+                        </Select>
+                    </div>
+                </div>
+            </div>
+
+            {/* image upload */}
+            <div className="form-row">
+                <div className="col">
+                    <div className="form-group d-grid">
+                        <label
+                            className="btn btn-outline-secondary text-left">
+                            {values.loading ? ' Uploading' : 'Image Upload'}
+                            <input
+                                type="file"
+                                name='image'
+                                accept='image/*'
+                                onChange={handleChange}
+                                hidden
+                            />
+                        </label>
+                    </div>
+                </div>
+            </div>
+
+            {/* button */}
+            <div className="row">
+                <div className="col d-grid">
+                    <Button
+                        onClick={handleSubmit}
+                        disabled={values.loading || values.uploading}
+                        className='btn btn-primary'
+                        loading={values.loading}
+                        icon={<SaveOutlined/>}
+                        type='primary'
+                        size='large'
+                        shape='round'
+                    >
+                        {values.loading ? 'Saving...' : 'Save & Continue'}
+                    </Button>
+                </div>
+            </div>
+        </form>
+    )
+
     return (<InstructorRoute>
         <main>
             <section className="py-5 text-center container">
