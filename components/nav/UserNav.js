e126b55d09b5d0adc9a4f1c93de4c1987cdbd9e7
Joseph Dubon <josephdubon@pm.me>
2022-03-25 20:06:13 -0700

    added conditional to add an 'active' class to current page

--- components/nav/UserNav.js
+++ components/nav/UserNav.js
@@ -1,10 +1,26 @@
 import Link from 'next/link'
+import {useContext, useEffect, useState} from "react";
+import {Context} from "../../context";
+import useRouter from "next/router";
 
 const UserNav = () => {
+    // set state for current page/link
+    const [currentPage, setCurrentPage] = useState('')
+    const isServer = () => typeof window !== 'undefined'
+    const {state, dispatch} = useContext(Context)
+    const {user} = state
+
+    const router = useRouter
+
+    // update state on page change
+    useEffect(() => {
+        // process.browser is depreciated use typeof instead
+        isServer() && setCurrentPage(window.location.pathname)
+    }, [isServer() && window.location.pathname])
     return (<>
-        <div className='nav flex-column nav-pills mt-2'>
+        <div className='nav flex-column nav-pills'>
             <Link href='/user'>
-                <a className='nav-link active'>Dashboard</a>
+                <a className={`nav-link ${currentPage === '/user' && 'active'}`}>Dashboard</a>
             </Link>
         </div>
     </>)
