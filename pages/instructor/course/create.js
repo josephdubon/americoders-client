961f516ef9ce787512849b026228d3f6506ab936
Joseph Dubon <josephdubon@pm.me>
2022-03-26 09:25:46 -0700

    added dynamic form from createForm function

--- pages/instructor/course/create.js
+++ pages/instructor/course/create.js
@@ -24,10 +24,8 @@ const CreateCourse = () => {
 
             <div className="album py-5 bg-light">
                 <div className='container'>
-                    <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
-                        <p className='lead'>
-                            Create Course
-                        </p>
+                    <div className='container-fluid col-md-4 offset-md-4 pb-5'>
+                        {courseCreateForm()}
                     </div>
                 </div>
             </div>
