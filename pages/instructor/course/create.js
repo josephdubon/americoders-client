82ff00083326b7b0f3a9601d03d55f7cc4f963ca
Joseph Dubon <josephdubon@pm.me>
2022-03-26 17:08:52 -0700

    added uploadButtonText to props for CourseCreateForm

--- pages/instructor/course/create.js
+++ pages/instructor/course/create.js
@@ -103,6 +103,7 @@ const CreateCourse = () => {
                             values={values}
                             setValues={setValues}
                             preview={preview}
+                            uploadButtonText={uploadButtonText}
                         />
                     </div>
                     <pre>{JSON.stringify(values, null, 4)}</pre>
