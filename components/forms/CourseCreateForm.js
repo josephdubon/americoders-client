fd05e8e5f98fc1f7a94c5653d160c058ad347679
Joseph Dubon <josephdubon@pm.me>
2022-03-28 13:53:49 -0700

    updated setValues and added price of 0 for free/unpaid courses

--- components/forms/CourseCreateForm.js
+++ components/forms/CourseCreateForm.js
@@ -55,7 +55,7 @@ const CourseCreateForm = ({
                         <Select
                             size='large'
                             value={values.paid}
-                            onChange={(v) => setValues({...values, paid: !values.paid})}
+                            onChange={(v) => setValues({...values, paid: v, price: 0})}
                         >
                             <Option value={true}>Paid</Option>
                             <Option value={false}>Free</Option>
