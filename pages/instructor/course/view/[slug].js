9285140c70744c2e2676a73fa0379aeb70b8ca23
Joseph Dubon <josephdubon@pm.me>
2022-03-29 19:54:49 -0700

    created new basic page template for single course view

---
+++ pages/instructor/course/view/[slug].js
@@ -0,0 +1,55 @@
+import {useEffect, useState} from 'react'
+import {useRouter} from 'next/router'
+import axios from 'axios'
+import InstructorRoute from '../../../../components/routes/InstructorRoute'
+
+const CourseView = () => {
+// state
+    const [course, setCourse] = useState({})
+
+    // router config
+    const router = useRouter()
+    // get slug from router url
+    const {slug} = router.query
+
+    // load requested course by slug
+    const loadCourse = async () => {
+        // make get request
+        const {data} = await axios.get(`/api/course/${slug}`)
+        // update state with course
+        setCourse(data)
+    }
+
+    useEffect(() => {
+        loadCourse()
+    }, [slug]) // use slug as dependency to run loadCourse in useEffect
+
+    return (
+        <InstructorRoute>
+            <main>
+                <section className='py-5 text-center container'>
+                    <div className='row py-lg-5'>
+                        <div className='col-lg-6 col-md-8 mx-auto'>
+                            <h1 className='fw-light'>{slug}</h1>
+                            <p className='lead text-muted'>...</p>
+                        </div>
+                    </div>
+                </section>
+
+                <div className='album py-5 bg-light'>
+                    <div className='container'>
+                        {/* Login Form */}
+                        <div className='container-fluid col-md-4 offset-md-4 pb-5'>
+
+                            <small>{JSON.stringify(course, null, 4)}</small>
+
+                        </div>
+                    </div>
+                </div>
+            </main>
+        </InstructorRoute>
+    )
+
+}
+
+export default CourseView
