09e58dc20e83f241b80dbc5860d4a298c0bddcf7
Joseph Dubon <josephdubon@pm.me>
2022-03-29 19:13:20 -0700

    created map to list inctructor courses, added basic layout with link an image and a user message

--- pages/instructor/index.js
+++ pages/instructor/index.js
@@ -48,10 +48,57 @@ const InstructorIndex = () => {
 
             <div className='album py-5 bg-light'>
                 <div className='container'>
-                    <div className='row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3'>
-                        <p className='small'>
-                            {JSON.stringify(courses, null, 4)}
-                        </p>
+                    <div className='row row-cols-1 row-cols-sm-1 row-cols-md-1 g-1'>
+                        {/* list all courses */}
+                        {courses && courses.map(course => (<>
+                            {/* parent media div */}
+                            <div className='d-flex align-items-center pt-2'>
+                                {/* image media div */}
+                                <div className="flex-shrink-0">
+                                    {/* image source */}
+                                    <Avatar
+                                        size={80}
+                                        src={course.image ? course.image.Location : '/americoders-course.png'}
+                                    />
+                                </div>
+                                {/* media text body */}
+                                <div className="flex-grow-1 ms-3">
+                                    {/* title / link to course*/}
+                                    <Link
+                                        href={`/instructor/course/view/${course._id}`}
+                                    >
+                                        <a className='mt-2 text-primary'><h5 className="pt-2">{course.name}</h5></a>
+                                    </Link>
+                                    <p>{
+                                        // show number of lessons in course
+                                        course.lessons.length} Lessons</p>
+                                    {
+                                        // show requirements message
+                                        course.lessons.length < 5 ? (
+                                                <p style={myStyle} className='text-warning'>At least 5 lessons are required
+                                                    to publish a course.</p>
+                                            ) :
+                                            // show success message
+                                            course.published ? (
+                                                    <p style={myStyle} className='text-success'>Your course is live in the
+                                                        marketplace.</p>
+                                                ) :
+                                                // show 'ready to publish' message
+                                                (
+                                                    <p style={myStyle} className='text-success'>Your course is ready to
+                                                        be published.</p>
+                                                )}
+                                </div>
+                                <div className='row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3'>
+                                    {course.published ? (
+                                        <div><CheckCircleOutlined className='h5 text-success'/></div>
+                                    ) : (
+                                        <div><CloseCircleOutlined className='h5 text-warning'/></div>
+
+                                    )}
+                                </div>
+                            </div>
+                        </>))}
                     </div>
                 </div>
             </div>
