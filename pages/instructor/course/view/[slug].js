48f66f7aead584aa63abe9b318f71e271c69bacc
Joseph Dubon <josephdubon@pm.me>
2022-03-30 11:54:28 -0700

    added course name to page header area

--- pages/instructor/course/view/[slug].js
+++ pages/instructor/course/view/[slug].js
@@ -38,8 +38,8 @@ const CourseView = () => {
                 <section className='py-5 text-center container'>
                     <div className='row py-lg-5'>
                         <div className='col-lg-6 col-md-8 mx-auto'>
-                            <h1 className='fw-light'>{slug}</h1>
-                            <p className='lead text-muted'>...</p>
+                            <h1 className='fw-light'>{course && course.name}</h1>
+                            <p className='lead text-muted'>{course && course.description}</p>
                         </div>
                     </div>
                 </section>
