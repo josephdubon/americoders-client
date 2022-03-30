dcc780a623640c551b8991671a6ac50690b24d58
Joseph Dubon <josephdubon@pm.me>
2022-03-30 12:27:12 -0700

    removed intro message from header

--- pages/forgot-password.js
+++ pages/forgot-password.js
@@ -116,9 +116,7 @@ const ForgotPassword = () => {
                 <div className='row py-lg-5'>
                     <div className='col-lg-6 col-md-8 mx-auto'>
                         <h1 className='fw-light'>Forgot Password</h1>
-                        <p className='lead text-muted'>Something short and leading about the collection below—its
-                            contents, the creator, etc. Make it short and sweet, but not too short so folks don’t simply
-                            skip over it entirely.</p>
+                        
                     </div>
                 </div>
             </section>
--- pages/index.js
+++ pages/index.js
@@ -5,9 +5,7 @@ const Index = () => {
                 <div className='row py-lg-5'>
                     <div className='col-lg-6 col-md-8 mx-auto'>
                         <h1 className='fw-light'>Home</h1>
-                        <p className='lead text-muted'>Something short and leading about the collection below—its
-                            contents, the creator, etc. Make it short and sweet, but not too short so folks don’t simply
-                            skip over it entirely.</p>
+                        
                     </div>
                 </div>
             </section>
