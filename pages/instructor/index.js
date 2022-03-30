a728c76f096c97f23dfbb5619700296cec9e1714
Joseph Dubon <josephdubon@pm.me>
2022-03-29 19:10:57 -0700

    updated imports: Link, Avatar, icons

--- pages/instructor/index.js
+++ pages/instructor/index.js
@@ -2,6 +2,9 @@ import {useContext, useEffect, useState} from 'react'
 import axios from 'axios'
 import {Context} from '../../context'
 import InstructorRoute from '../../components/routes/InstructorRoute'
+import {Avatar} from 'antd'
+import Link from 'next/link'
+import {CheckCircleOutlined, CloseCircleOutlined} from '@ant-design/icons'
 
 const InstructorIndex = () => {
     // state
