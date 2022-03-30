699684da0f3c747f89d673669e2a7cd8a1e3ddfe
Joseph Dubon <josephdubon@pm.me>
2022-03-30 12:09:12 -0700

    indentation fix

--- pages/instructor/course/view/[slug].js
+++ pages/instructor/course/view/[slug].js
@@ -28,64 +28,62 @@ const CourseView = () => {
 
     // style
     const myStyle = {
-        marginTop: '-15px',
-        fontSize: '10px',
+        marginTop: '-15px', fontSize: '10px',
     }
 
-    return (
-        <InstructorRoute>
-            <main>
-                <section className='py-5 text-center container'>
-                    <div className='row py-lg-5'>
-                        <div className='col-lg-6 col-md-8 mx-auto'>
-                            <h1 className='fw-light'>{course && course.name}</h1>
-                            <p className='lead text-muted'>{course && course.description}</p>
-                        </div>
+    return (<InstructorRoute>
+        <main>
+            <section className='py-5 text-center container'>
+                <div className='row py-lg-5'>
+                    <div className='col-lg-6 col-md-8 mx-auto'>
+                        <h1 className='fw-light'>{course && course.name}</h1>
+                        <p className='lead text-muted'>{course && course.description}</p>
                     </div>
-                </section>
+                </div>
+            </section>
 
-                <div className='album py-5 bg-light'>
-                    <div className='container'>
-                        <div className='row row-cols-1 row-cols-sm-1 row-cols-md-1 g-1'>
-                            {course && (<>
-                                    <div className='d-flex align-items-center pt-2'>
-                                        {/* image media div */}
-                                        <div className="flex-shrink-0">
-                                            {/* image source */}
-                                            <Avatar
-                                                size={80}
-                                                src={course.image ? course.image.Location : '/images/americoders-course.png'}
-                                            />
-                                        </div>
+            <div className='album py-5 bg-light'>
+                <div className='container'>
+                    <div className='row row-cols-1 row-cols-sm-1 row-cols-md-1 g-1'>
+                        {course && (<>
+                            <div className='d-flex align-items-center pt-2'>
+                                {/* image media div */}
+                                <div className="flex-shrink-0">
+                                    {/* image source */}
+                                    <Avatar
+                                        size={80}
+                                        src={course.image ? course.image.Location : '/images/americoders-course.png'}
+                                    />
+                                </div>
 
-                                        {/* media text body */}
-                                        <div className="flex-grow-1 ms-3">
-                                            {/* course name and lesson count */}
-                                            <h5 className="pt-2">{course.name}</h5>
-                                            <p> {course.lessons && course.lessons.length} Lessons</p>
+                                {/* media text body */}
+                                <div className="flex-grow-1 ms-3">
+                                    {/* course name and lesson count */}
+                                    <h5 className="pt-2">{course.name}</h5>
+                                    <p> {course.lessons && course.lessons.length} Lessons</p>
 
-                                            <p style={myStyle}>{course.category}</p>
-                                        </div>
+                                    <p style={myStyle}>{course.category}</p>
+                                </div>
 
-                                        {/* action icons */}
-                                        <div className='row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3'>
-                                            {/* edit */}
-                                            <div>
-                                                <Tooltip title='Edit'>
-                                                    <EditOutlined className='h5 text-warning'/>
-                                                    <small>Edit</small>
-                                                </Tooltip>
-                                            </div>
+                                {/* action icons */}
+                                <div className='row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3'>
+                                    {/* edit */}
+                                    <div>
+                                        <Tooltip title='Edit'>
+                                            <EditOutlined className='h5 text-warning'/>
+                                            <small>Edit</small>
+                                        </Tooltip>
+                                    </div>
 
-                                            <span/> {/* keep this here for some space */}
+                                    <span/> {/* keep this here for some space */}
 
-                                            {/* publish */}
-                                            <div>
-                                                <Tooltip title='Publish'>
-                                                    <CheckOutlined className='h5 text-danger'/>
-                                                    <small>Publish</small>
-                                                </Tooltip>
-                                            </div>
+                                    {/* publish */}
+                                    <div>
+                                        <Tooltip title='Publish'>
+                                            <CheckOutlined className='h5 text-danger'/>
+                                            <small>Publish</small>
+                                        </Tooltip>
+                                    </div>
 
                                 </div>
                             </div>
@@ -96,12 +94,11 @@ const CourseView = () => {
                             </div>
                         </>)}
 
-                        </div>
                     </div>
                 </div>
-            </main>
-        </InstructorRoute>
-    )
+            </div>
+        </main>
+    </InstructorRoute>)
 
 }
 
