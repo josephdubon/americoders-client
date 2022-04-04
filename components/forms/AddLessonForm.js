7c7003b34114d7c9dbeed4668de00d4ed164b4e3
Joseph Dubon <josephdubon@pm.me>
2022-04-04 11:43:53 -0700

    created new file for adding lessons to course

---
+++ components/forms/AddLessonForm.js
@@ -0,0 +1,46 @@
+import {Button} from 'antd'
+
+const AddLessonForm = ({values, setValues, handleAddLesson, uploading}) => {
+    return (<>
+        <div className='container pt-3'>
+
+            <form onSubmit={handleAddLesson}>
+                <input
+                    type='text'
+                    className='form-control'
+                    onChange={(e) => setValues({...values, title: e.target.value})}
+                    value={values.title}
+                    placeholder='Title'
+                    autoFocus
+                    required
+                />
+
+                <textarea
+                    className='form-control mt-3'
+                    cols='7'
+                    rows='7'
+                    onChange={(e) => setValues({...values, content: e.target.value})}
+                    value={values.content}
+                    placeholder='Content'
+                >
+            </textarea>
+
+
+                <div className='row'>
+                    <div className='col d-grid'>
+                        <Button
+                            onClick={handleAddLesson}
+                            className='col mt-3'
+                            size='large'
+                            type='primary'
+                            shape='round'
+                            loading={uploading}
+                        >Save</Button>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </>)
+}
+
+export default AddLessonForm
