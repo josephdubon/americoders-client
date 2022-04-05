7961fc3671ec7b49d0d59aba823176a1ed53f287
Joseph Dubon <josephdubon@pm.me>
2022-04-05 13:20:00 -0700

    fixed double to single quotes

--- pages/instructor/course/create.js
+++ pages/instructor/course/create.js
@@ -147,15 +147,15 @@ const CreateCourse = () => {
 
     return (<InstructorRoute>
         <main>
-            <section className="py-5 text-center container">
-                <div className="row py-lg-5">
-                    <div className="col-lg-6 col-md-8 mx-auto">
-                        <h1 className="fw-light">Create Course</h1>
+            <section className='py-5 text-center container'>
+                <div className='row py-lg-5'>
+                    <div className='col-lg-6 col-md-8 mx-auto'>
+                        <h1 className='fw-light'>Create Course</h1>
                     </div>
                 </div>
             </section>
 
-            <div className="album py-5 bg-light">
+            <div className='album py-5 bg-light'>
                 <div className='container'>
                     <div className='container-fluid col-md-4 offset-md-4 pb-5'>
                         {/* use props for form function and values */}
