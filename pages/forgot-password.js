28496ff2f272ce7d290afdcf05865404c60d52e0
Joseph Dubon <josephdubon@pm.me>
2022-04-05 13:20:21 -0700

    added reformatting to code

--- pages/forgot-password.js
+++ pages/forgot-password.js
@@ -116,7 +116,7 @@ const ForgotPassword = () => {
                 <div className='row py-lg-5'>
                     <div className='col-lg-6 col-md-8 mx-auto'>
                         <h1 className='fw-light'>Forgot Password</h1>
-                        
+
                     </div>
                 </div>
             </section>
