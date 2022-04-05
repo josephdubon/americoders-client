8652521afab2301ae2e7fc7fdf815e914a15adee
Joseph Dubon <josephdubon@pm.me>
2022-04-05 13:21:08 -0700

    fixed double to single quotes

--- pages/index.js
+++ pages/index.js
@@ -10,9 +10,9 @@ const Index = () => {
                 </div>
             </section>
 
-            <div className="album py-5 bg-light">
-                <div className="container">
-                    <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+            <div className='album py-5 bg-light'>
+                <div className='container'>
+                    <div className='row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3'>
                         <p className='lead'>
                             Welcome Home
                         </p>
