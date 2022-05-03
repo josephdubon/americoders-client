d4f9a8a082f4db8677c94df2546914389e23d931
Joseph Dubon <josephdubon@pm.me>
2022-05-03 09:25:38 -0700

    added comment to modal view

--- pages/instructor/course/edit/[slug].js
+++ pages/instructor/course/edit/[slug].js
@@ -300,7 +300,7 @@ const EditCourse = () => {
                                     >
                                         <Item.Meta
                                             onClick={() => {
-                                                //
+                                                // update lesson modal
                                                 setVisible(true)
                                                 setCurrent(item.title)
                                             }}
