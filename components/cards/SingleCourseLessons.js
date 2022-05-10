52eb515a92c7be496cd85ca19d55f8e382a5fad0
Joseph Dubon <josephdubon@pm.me>
2022-05-09 17:21:46 -0700

    created new component to list lessons in course single view, renders list from props

---
+++ components/cards/SingleCourseLessons.js
@@ -0,0 +1,32 @@
+import {Avatar, List} from 'antd'
+
+const {Item} = List
+
+const SingleCourseLessons = ({lessons, setPreview, setShowModal, showModal}) => {
+    return (<>
+        <div className='container'>
+            <div className='row'>
+                <div className='col lesson-list'>
+                    {lessons && <h4>{lessons.length} Lessons</h4>}
+                    <hr/>
+                    <List
+                        itemLayout='horizontal'
+                        dataSource={lessons}
+                        renderItem={(item, index) => (
+                            <Item>
+                                <Item.Meta
+                                    avatar={<Avatar>{index + 1}</Avatar>}
+                                    title={item.title}
+                                >
+
+                                </Item.Meta>
+                            </Item>
+                        )}
+                    />
+                </div>
+            </div>
+        </div>
+    </>)
+}
+
+export default SingleCourseLessons
