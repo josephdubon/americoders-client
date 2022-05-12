27be81641a4625c00749c434575f55f64c08be8e
Joseph Dubon <josephdubon@pm.me>
2022-05-12 13:24:25 -0700

    added useContext

--- pages/course/[slug].js
+++ pages/course/[slug].js
@@ -1,4 +1,4 @@
-import {useState} from 'react'
+import {useContext, useState} from 'react'
 import axios from 'axios'
 import {useRouter} from 'next/router'
 import SingleCourseHero from '../../components/cards/SingleCourseHero'
