ea620c2c1afce94e68a9fa4903a35eac3629c76d
Joseph Dubon <josephdubon@pm.me>
2022-05-13 16:14:19 -0700

    created cancel page for failed Stripe payments

---
+++ pages/stripe/cancel.js
@@ -0,0 +1,20 @@
+import UserRoute from '../../components/routes/UserRoute'
+import {CloudSyncOutlined} from '@ant-design/icons'
+
+const StripeCancel = () => {
+    return (
+        <UserRoute showNav={false}>
+            <div className='row text-center'>
+                <div className='col-md-9'>
+                    <CloudSyncOutlined className='display-1 text-danger p-5'/>
+                    <p className='lead'>
+                        Payment failed. Try again.
+                    </p>
+                </div>
+                <div className='col-md-3'>{''}</div>
+            </div>
+        </UserRoute>
+    )
+}
+
+export default StripeCancel
