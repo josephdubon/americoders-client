e2a06ed263a1ec216409205f67a847fb4e4c0751
Joseph Dubon <josephdubon@pm.me>
2022-05-15 16:33:53 -0700

    added logic to load course data

--- pages/user/course/[slug].js
+++ pages/user/course/[slug].js
@@ -1,4 +1,6 @@
+import {useEffect, useState} from 'react'
 import {useRouter} from 'next/router'
+import axios from 'axios'
 
 const SingleCourse = () => {
     const [loading, setLoading] = useState(false)
@@ -6,6 +8,19 @@ const SingleCourse = () => {
 
     // router
     const router = useRouter()
+    const {slug} = router.query
+
+    useEffect(() => {
+        if (slug) loadCourse()
+    }, [slug])
+
+    const loadCourse = async () => {
+        // collect data
+        const {data} = await axios.get(`/api/course/${slug}`)
+
+        // update state
+        setCourse(data)
+    }
 
     return (<>
         Course Slug:
