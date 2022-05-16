640590f86053dea2803d736ccdb5fa985c424575
Joseph Dubon <josephdubon@pm.me>
2022-05-15 17:03:01 -0700

    created new student route to protect course views from non-students

---
+++ components/routes/StudentRoute.js
@@ -0,0 +1,45 @@
+import {useEffect, useState} from 'react'
+import axios from 'axios'
+import {useRouter} from 'next/router'
+import {SyncOutlined} from '@ant-design/icons'
+
+const StudentRoute = ({children, showNav = true}) => {
+    // state
+    const [ok, setOk] = useState(false)
+
+    // router
+    const router = useRouter()
+
+    useEffect(() => {
+        fetchUser()
+    }, [])
+
+    const fetchUser = async () => {
+        // try to get user match
+        try {
+            const {data} = await axios.get('/api/current-user')
+            if (data.ok) setOk(true) // if match found, set ok to true
+        } catch (err) {
+            console.log(err)
+            setOk(false) // if no match found, set ok to false
+            await router.push('/login') // redirect to login page
+        }
+    }
+
+    return (<>
+        {!ok ? (
+            // if ok is false, show loading screen
+            <SyncOutlined spin className='d-flex justify-content-center display-1 text-primary p-5'
+            />
+        ) : (
+            // if ok is true, show child element
+            <>
+                <div className='container-fluid'>
+                    {children}
+                </div>
+            </>
+        )}
+    </>)
+}
+
+export default StudentRoute
