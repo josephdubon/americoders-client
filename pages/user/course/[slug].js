39c07e44945f2874769c2b4f58ebaa79440ba2d4
Joseph Dubon <josephdubon@pm.me>
2022-05-15 20:24:23 -0700

    created markComplete function to mark lessons complete

--- pages/user/course/[slug].js
+++ pages/user/course/[slug].js
@@ -32,36 +32,41 @@ const SingleCourse = () => {
         setCourse(data)
     }
 
-    return (
-        <StudentRoute>
-            <div className='row'>
-                <div
-                    style={{maxWidth: 320}}
+    const markComplete = async () => {
+        const {data} = await axios.post(`/api/mark-complete`, {
+            courseId: course._id,
+            lessonId: course.lessons[clicked]._id
+        })
+        console.log(data)
+    }
+
+    return (<StudentRoute>
+        <div className='row'>
+            <div
+                style={{maxWidth: 320}}
+            >
+                <Button
+                    disabled={loading}
+                    onClick={() => setCollapsed(!collapsed)}
+                    className='text-primary mt-1 btn-block mb-2'
                 >
-                    <Button
-                        disabled={loading}
-                        onClick={() => setCollapsed(!collapsed)}
-                        className='text-primary mt-1 btn-block mb-2'
+                    {createElement(collapsed ? MenuUnfoldOutlined : MenuFoldOutlined)}{' '}
+                    {!collapsed && 'Lessons'}
+                </Button>
+                <Menu
+                    defaultSelectedKeys={[clicked]}
+                    inlineCollapsed={collapsed}
+                    style={{height: '80vh', overflow: 'scroll'}}
+                >
+                    {course.lessons.map((lesson, index) => (<Item
+                        onClick={() => setClicked(index)}
+                        key={index}
+                        icon={<Avatar>{index}</Avatar>}
                     >
-                        {createElement(collapsed ? MenuUnfoldOutlined : MenuFoldOutlined)}{' '}
-                        {!collapsed && 'Lessons'}
-                    </Button>
-                    <Menu
-                        defaultSelectedKeys={[clicked]}
-                        inlineCollapsed={collapsed}
-                        style={{height: '80vh', overflow: 'scroll'}}
-                    >
-                        {course.lessons.map((lesson, index) => (
-                            <Item
-                                onClick={() => setClicked(index)}
-                                key={index}
-                                icon={<Avatar>{index}</Avatar>}
-                            >
-                                {lesson.title.substring(0, 30)}
-                            </Item>
-                        ))}
-                    </Menu>
-                </div>
+                        {lesson.title.substring(0, 30)}
+                    </Item>))}
+                </Menu>
+            </div>
 
                 <div className='col'>
                     {clicked !== -1 ? (
