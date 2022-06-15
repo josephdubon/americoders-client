6759619864c1c6300b1aeaa82e39bff657b0be33
Joseph Dubon <josephdubon@pm.me>
2022-06-15 13:30:39 -0700

    removed hard coded form and added it as its own component

--- pages/forgot-password.js
+++ pages/forgot-password.js
@@ -1,178 +1,60 @@
-import {useContext, useEffect, useState} from 'react'
-import {Context} from '../context'
-import {useRouter} from 'next/router'
-import {SyncOutlined} from '@ant-design/icons';
+import Link from 'next/link'
+import {Image, Layout} from 'antd'
 
-const axios = require('axios')
-const {toast} = require('react-toastify')
+const {Content} = Layout
 
-const ForgotPassword = () => {
-    // state
-    const [email, setEmail] = useState('')
-    const [success, setSuccess] = useState(false)
-    const [code, setCode] = useState('')
-    const [newPassword, setNewPassword] = useState('')
-    const [loading, setLoading] = useState(false)
+const ForgotPasswordForm = () => {
 
-    // context
-    const {
-        state: {user},
-    } = useContext(Context)
-
-    // router
-    const router = useRouter()
-
-    // if user is logged-in, redirect to homepage
-    useEffect(() => {
-        if (user !== null) router.push('/')
-    }, [user]) // add user as dependency to block page from logged-in user
-
-    // submit form data to backend
-    const handleSubmit = async (e) => {
-        e.preventDefault()
-        try {
-            setLoading(true)
-
-            // make request to server
-            const {data} = await axios.post('/api/forgot-password', {email})
-
-            // set success state and give user notification
-            setSuccess(true)
-
-            toast('Check your email for the secret code', {
-                position: 'top-center',
-                autoClose: 5000,
-                hideProgressBar: false,
-                closeOnClick: true,
-                pauseOnHover: true,
-                draggable: true,
-                progress: undefined,
-            })
-            setLoading(false)
-
-        } catch (err) {
-            setLoading(false)
-
-            // notification config
-            toast(err.response.data, {
-                position: 'top-center',
-                autoClose: 5000,
-                hideProgressBar: false,
-                closeOnClick: true,
-                pauseOnHover: true,
-                draggable: true,
-                progress: undefined,
-            })
-        }
-    }
-
-    // submit new password from forgotPassword
-    const handleResetPassword = async (e) => {
-        e.preventDefault()
-        try {
-            setLoading(true)
-            const {data} = await axios.post('/api/reset-password', {
-                email, code, newPassword,
-            })
-
-            // reset fields to empty
-            setEmail('')
-            setCode('')
-            setNewPassword('')
-            setLoading(false)
-
-            // notification config
-            toast('Success! Please try to login with your new password', {
-                position: 'top-center',
-                autoClose: 5000,
-                hideProgressBar: false,
-                closeOnClick: true,
-                pauseOnHover: true,
-                draggable: true,
-                progress: undefined,
-            })
-
-            // redirect to login page
-            await router.push('login')
-        } catch (err) {
-            setLoading(false)
-
-            // notification config
-            toast(err.response.data, {
-                position: 'top-center',
-                autoClose: 5000,
-                hideProgressBar: false,
-                closeOnClick: true,
-                pauseOnHover: true,
-                draggable: true,
-                progress: undefined,
-            })
-        }
-    }
 
     return (<>
-        <main>
-            <section className='py-5 text-center container'>
-                <div className='row py-lg-5'>
-                    <div className='col-lg-6 col-md-8 mx-auto'>
-                        <h1 className='fw-light'>Forgot Password</h1>
-
+        {/* hero section */}
+        <Content className='bg-light'>
+            <div className='container col-xxl-8 px-4 py-5'>
+                <div className='row flex-lg-row-reverse align-items-center justify-content-center g-5 py-5'>
+                    <div className='col-10 col-sm-8 col-lg-6'>
+                        <Image
+                            src='/images/branding/bg-images/americoders-student-female-laptop-fun.jpg'
+                            alt='Americoders'
+                            loading='lazy'
+                            preview={false}
+                        />
+                        <p className=' text-muted mt-2'>
+                            Thank you for your patience as we build out our system. We are currently in development of
+                            our courses/workshops. Please keep checking back for new content!
+                        </p>
                     </div>
-                </div>
-            </section>
+                    <div className='col-lg-6'>
+                        <h1 className='display-5 fw-bold lh-1 mb-3'>ForgotPasswordForm</h1>
+                        <p className='lead text-muted'>
+                            Welcome back, <strong className='text-primary fw-bold'>Americoder</strong>!
+                        </p>
+                        <p className=' text-muted mt-2'>
+                            Thanks for visiting again! Always remember to reach out if you have any questions or
+                            comments.
+                        </p>
 
-            <div className='album py-5 bg-light'>
-                <div className='container'>
-                    {/* ForgotPassword Form */}
-                    <div className='container-fluid col-md-4 offset-md-4 pb-5'>
+                        {/* ForgotPasswordForm Form */}
+                        <div className='col-md-12 offset-md-12 pb-5'>
 
-                        {/* if success is true then handle submit with handleResetPassword */}
-                        <form onSubmit={success ? handleResetPassword : handleSubmit}>
-                            <input
-                                type='email'
-                                className='form-control mb-4 p-4'
-                                value={email}
-                                onChange={e => setEmail(e.target.value)}
-                                placeholder='Enter email'
-                                required
-                            />
+                            {/* login form */}
+                            <ForgotPasswordForm/>
+                            <br/>
 
-                            {/* render on successful reset password submission */}
-                            {success && <>
-                                <input
-                                    type='password'
-                                    className='form-control mb-4 p-4'
-                                    value={code}
-                                    onChange={e => setCode(e.target.value)}
-                                    placeholder='Enter secret code'
-                                    required
-                                />
+                            {/*  register */}
+                            <p className='form-text text-center p3'>
+                                Want to sign-up? <Link href={'/register'}><a>Register</a></Link>
+                                <br/>
 
-                                <input
-                                    type='password'
-                                    className='form-control mb-4 p-4'
-                                    value={newPassword}
-                                    onChange={e => setNewPassword(e.target.value)}
-                                    placeholder='Enter new password'
-                                    required
-                                />
-                            </>}
+                                {/* reset password */}
+                                <Link href={'/forgot-password'}><a className='text-danger'>Forgot password?</a></Link>
+                            </p>
+                        </div>
 
-                            <div className='d-grid gap-2'>
-                                <button
-                                    type='submit'
-                                    className='btn btn-primary'
-                                    disabled={!email || loading}
-                                >
-                                    {loading ? <SyncOutlined spin/> : 'Submit'}
-                                </button>
-                            </div>
-                        </form>
                     </div>
                 </div>
             </div>
-        </main>
+        </Content>
     </>)
 }
 
-export default ForgotPassword
+export default ForgotPasswordForm
