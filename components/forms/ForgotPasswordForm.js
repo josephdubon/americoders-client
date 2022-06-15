b83b3d5308fe1849882d6884e44dc599e3ab3b30
Joseph Dubon <josephdubon@pm.me>
2022-06-15 13:27:29 -0700

    created new component for forgot password form

---
+++ components/forms/ForgotPasswordForm.js
@@ -0,0 +1,158 @@
+import {useContext, useEffect, useState} from 'react'
+import {Context} from '../../context'
+import {useRouter} from 'next/router'
+import axios from 'axios'
+import {toast} from 'react-toastify'
+import {SyncOutlined} from '@ant-design/icons'
+
+const ForgotPasswordForm = () => {
+// state
+    const [email, setEmail] = useState('')
+    const [success, setSuccess] = useState(false)
+    const [code, setCode] = useState('')
+    const [newPassword, setNewPassword] = useState('')
+    const [loading, setLoading] = useState(false)
+
+    // context
+    const {
+        state: {user},
+    } = useContext(Context)
+
+    // router
+    const router = useRouter()
+
+    // if user is logged-in, redirect to homepage
+    useEffect(() => {
+        if (user !== null) router.push('/')
+    }, [user]) // add user as dependency to block page from logged-in user
+
+    // submit form data to backend
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+        try {
+            setLoading(true)
+
+            // make request to server
+            const {data} = await axios.post('/api/forgot-password', {email})
+
+            // set success state and give user notification
+            setSuccess(true)
+
+            toast('Check your email for the secret code', {
+                position: 'top-center',
+                autoClose: 5000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+            })
+            setLoading(false)
+
+        } catch (err) {
+            setLoading(false)
+
+            // notification config
+            toast(err.response.data, {
+                position: 'top-center',
+                autoClose: 5000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+            })
+        }
+    }
+
+    // submit new password from forgotPassword
+    const handleResetPassword = async (e) => {
+        e.preventDefault()
+        try {
+            setLoading(true)
+            const {data} = await axios.post('/api/reset-password', {
+                email, code, newPassword,
+            })
+
+            // reset fields to empty
+            setEmail('')
+            setCode('')
+            setNewPassword('')
+            setLoading(false)
+
+            // notification config
+            toast('Success! Please try to login with your new password', {
+                position: 'top-center',
+                autoClose: 5000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+            })
+
+            // redirect to login page
+            await router.push('login')
+        } catch (err) {
+            setLoading(false)
+
+            // notification config
+            toast(err.response.data, {
+                position: 'top-center',
+                autoClose: 5000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+            })
+        }
+    }
+
+    return (<>
+        {/* if success is true then handle submit with handleResetPassword */}
+        <form onSubmit={success ? handleResetPassword : handleSubmit}>
+            <input
+                type='email'
+                className='form-control mb-4 p-4'
+                value={email}
+                onChange={e => setEmail(e.target.value)}
+                placeholder='Enter email'
+                required
+            />
+
+            {/* render on successful reset password submission */}
+            {success && <>
+                <input
+                    type='password'
+                    className='form-control mb-4 p-4'
+                    value={code}
+                    onChange={e => setCode(e.target.value)}
+                    placeholder='Enter secret code'
+                    required
+                />
+
+                <input
+                    type='password'
+                    className='form-control mb-4 p-4'
+                    value={newPassword}
+                    onChange={e => setNewPassword(e.target.value)}
+                    placeholder='Enter new password'
+                    required
+                />
+            </>}
+
+            <div className='d-grid gap-2'>
+                <button
+                    type='submit'
+                    className='btn btn-primary'
+                    disabled={!email || loading}
+                >
+                    {loading ? <SyncOutlined spin/> : 'Submit'}
+                </button>
+            </div>
+        </form>
+    </>)
+}
+
+export default ForgotPasswordForm
