f37f5cac1440418c2ef1943ba45bf790c479400e
Joseph Dubon <josephdubon@pm.me>
2022-06-24 11:45:07 -0700

    added new background images

---
+++ components/effects/TessaractCube.js
@@ -0,0 +1,13 @@
+export default function () {
+
+    return (<>
+        <div className='cube'>
+            <div className='top'>{''}</div>
+            <div className='right'>{''}</div>
+            <div className='bottom'>{''}</div>
+            <div className='left'>{''}</div>
+            <div className='front'>{''}</div>
+            <div className='back'>{''}</div>
+        </div>
+    </>)
+}
