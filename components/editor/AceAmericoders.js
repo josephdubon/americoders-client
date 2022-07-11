615bd692dd81ef3c50a09c402a851e452d6c594b
Joseph Dubon <josephdubon@pm.me>
2022-07-11 15:57:22 -0700

    changed theme to dracula

--- components/editor/AceAmericoders.js
+++ components/editor/AceAmericoders.js
@@ -3,7 +3,7 @@ import AceEditor from 'react-ace'
 import 'ace-builds/src-noconflict/mode-xml' // works just like HTML
 import 'ace-builds/src-noconflict/mode-css'
 import 'ace-builds/src-noconflict/mode-javascript'
-import 'ace-builds/src-noconflict/theme-monokai'
+import 'ace-builds/src-noconflict/theme-dracula'
 import 'ace-builds/src-noconflict/ext-language_tools'
 
 const AceBasic = (props) => {
@@ -29,7 +29,7 @@ const AceBasic = (props) => {
             <AceEditor
                 value={value}
                 mode={language}
-                theme='monokai'
+                theme='dracula'
                 onChange={onChange}
                 name={editorName}
                 editorProps={{$blockScrolling: true}}
