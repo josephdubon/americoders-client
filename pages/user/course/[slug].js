3dddfb1d442e97c6767b0d6613f9fe6190bc1695
Joseph Dubon <josephdubon@pm.me>
2022-07-11 14:01:59 -0700

    added code render area

--- pages/user/course/[slug].js
+++ pages/user/course/[slug].js
@@ -202,6 +202,17 @@ const SingleCourse = () => {
                                 <AceDynamic/>
                             </div>
 
+                            {/* iframe render area */}
+                            <div className='editorArea'>
+                                <iframe
+                                    title={'output'}
+                                    sandbox={'allow-scripts'}
+                                    frameBorder={'0'}
+                                    width={'100%'}
+                                    height={'100%'}
+                                />
+                            </div>
+
                         </>
                     ) : (
                         <div className='d-flex justify-content-center p-5'>
