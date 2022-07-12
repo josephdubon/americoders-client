ef0342fc00511e2668021db3f0f449e79c8781a5
Joseph Dubon <josephdubon@pm.me>
2022-07-11 20:07:05 -0700

    added state for editor changes

--- pages/user/course/[slug].js
+++ pages/user/course/[slug].js
@@ -37,7 +37,7 @@ const SingleCourse = () => {
     const [html, setHtml] = useState('')
     const [css, setCss] = useState('')
     const [javascript, setJavascript] = useState('')
-
+    const [srcDoc, setSrcDoc] = useState('')
     // force stat update
     const [updateState, setUpdateState] = useState(false)
 
