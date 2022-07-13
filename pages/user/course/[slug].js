c98b1a5c73a8015bbd2b461388a067de257a6ca7
Joseph Dubon <josephdubon@pm.me>
2022-07-13 14:24:07 -0700

    removed unused state and unused components

--- pages/user/course/[slug].js
+++ pages/user/course/[slug].js
@@ -2,30 +2,18 @@ import {useEffect, useState} from 'react'
 import {useRouter} from 'next/router'
 import axios from 'axios'
 import StudentRoute from '../../../components/routes/StudentRoute'
-
-// next.js renders pages server-side, giving a 'window object isn't available' error
-// fix: dynamically import the module containing the AceEditor:
-import dynamic from 'next/dynamic'
 import {Avatar, Button, Drawer, Image, Layout, List, Menu, Row, Space} from 'antd'
-
 import ReactMarkdown from 'react-markdown'
 import {CheckCircleFilled, MinusCircleFilled, SyncOutlined} from '@ant-design/icons'
 import ReactPlayer from 'react-player'
 import PlaygroundFrontEnd from '../../../components/editor/PlaygroundFrontEnd'
 
 const {Content} = Layout
-
-const AceDynamic = dynamic(
-    () => import('../../../components/editor/AceAmericoders'),
-    {ssr: false}
-)
-
 const {Item} = Menu
 
 const SingleCourse = () => {
     // state
     const [clicked, setClicked] = useState(-1)
-    const [collapsed, setCollapsed] = useState(false)
     const [loading, setLoading] = useState(false)
     const [course, setCourse] = useState({lessons: []}) // course.lessonn
     const [completedLessons, setCompletedLessons] = useState([])
@@ -108,173 +96,170 @@ const SingleCourse = () => {
     }
 
     return (<>
-        {loading && (<SyncOutlined
-            spin
-            className='d-flex justify-content-between display-1 text-danger p-5 center'
-        />)}
+            {loading && (<SyncOutlined
+                spin
+                className='d-flex justify-content-between display-1 p-5 center'
+            />)}
 
-        <StudentRoute>
-
-            {/* top drawer lesson menu */}
-            <Space className='d-flex justify-content-center'>
-                <Button
-                    type='primary'
-                    onClick={showDrawer}
-                    className='text-primary mt-3 btn-block mb-3 text-white'
-                >
-                    {course.name} | Lessons Menu
-                </Button>
-            </Space>
-            <Drawer
-                title={course && course.name + ' | Lessons Menu'}
-                placement={'left'}
-                width={500}
-                onClose={onClose}
-                visible={visible}
-                extra={<Space>
-                    <Button type='primary' onClick={onClose}>
-                        Close
-                    </Button>
-                </Space>}
-            >
-                <Menu
-                    theme={'dark'}
-                    mode='inline'
-                    defaultSelectedKeys={[clicked]}
-                    inlineCollapsed={collapsed}
-                    className='p-3 pt-4'
-                    style={{height: '80vh', overflow: 'scroll'}}
-                >
-                    {course.lessons.map((lesson, index) => (<Item
-                        onClick={() => setClicked(index)}
-                        key={index}
-                        icon={<Avatar>{index + 1}</Avatar>}
+            <StudentRoute>
+                {/* top drawer lesson menu */}
+                <Space className='d-flex justify-content-center'>
+                    <Button
+                        type='primary'
+                        onClick={showDrawer}
+                        className='text-primary mt-3 btn-block mb-3 text-white'
                     >
+                        {course.name} | Lessons Menu
+                    </Button>
+                </Space>
+                <Drawer
+                    title={course && course.name + ' | Lessons Menu'}
+                    placement={'left'}
+                    width={500}
+                    onClose={onClose}
+                    visible={visible}
+                    extra={<Space>
+                        <Button type='primary' onClick={onClose}>
+                            Close
+                        </Button>
+                    </Space>}
+                >
+                    <Menu
+                        theme={'dark'}
+                        mode='inline'
+                        defaultSelectedKeys={[clicked]}
+                        className='p-3 pt-4'
+                        style={{height: '80vh', overflow: 'scroll'}}
+                    >
+                        {course.lessons.map((lesson, index) => (<Item
+                            onClick={() => setClicked(index)}
+                            key={index}
+                            icon={<Avatar>{index + 1}</Avatar>}
+                        >
                                 <span style={{marginRight: '25px'}}>
                                     {lesson.title.substring(0, 30)}
                                 </span>
-                        {completedLessons.includes(lesson._id) ? (<CheckCircleFilled
-                            className='float-end text-primary ml-2'
-                            style={{marginTop: '13px'}}
-                        />) : (<MinusCircleFilled
-                            className='float-end text-danger ml-2'
-                            style={{marginTop: '13px'}}
-                        />)}
-                    </Item>))}
-                </Menu>
-            </Drawer>
+                            {completedLessons.includes(lesson._id) ? (<CheckCircleFilled
+                                className='float-end text-primary ml-2'
+                                style={{marginTop: '13px'}}
+                            />) : (<MinusCircleFilled
+                                className='float-end text-danger ml-2'
+                                style={{marginTop: '13px'}}
+                            />)}
+                        </Item>))}
+                    </Menu>
+                </Drawer>
 
-            <Row>
+                <Row>
 
-                {/* main content area */}
-                <div className='col'>
-                    {/* top bar lesson title , completed status */}
-                    {clicked !== -1 ? (
-                        <>
-                            <Content className='bg-body'>
-                                <div className='container-fluid px-4 py-5'>
-                                    {/* lesson title */}
-                                    <h2 className='pb-2 border-bottom'>
-                                        {course.lessons[clicked].title.substring(0, 30)}
-                                    </h2>
+                    {/* main content area */}
+                    <div className='col'>
+                        {/* top bar lesson title , completed status */}
+                        {clicked !== -1 ? (
+                            <>
+                                <Content className='bg-body'>
+                                    <div className='container-fluid px-4 py-5'>
+                                        {/* lesson title */}
+                                        <h2 className='pb-2 border-bottom'>
+                                            {course.lessons[clicked].title.substring(0, 30)}
+                                        </h2>
 
-                                    {/* mark as complete area */}
-                                    {completedLessons.includes(course.lessons[clicked]._id) ? (
-                                        <span
-                                            className='float-end'
-                                            role='button'
-                                            onClick={markIncomplete}
-                                        >
+                                        {/* mark as complete area */}
+                                        {completedLessons.includes(course.lessons[clicked]._id) ? (
+                                            <span
+                                                className='float-end'
+                                                role='button'
+                                                onClick={markIncomplete}
+                                            >
                                         Mark as incomplete
                                     </span>
-                                    ) : (
-                                        <span
-                                            className='float-end'
-                                            role='button'
-                                            onClick={markComplete}>
+                                        ) : (
+                                            <span
+                                                className='float-end'
+                                                role='button'
+                                                onClick={markComplete}>
                                         Mark as completed
                                     </span>
-                                    )}
-
-                                    {/* course description*/}
-                                    <div className='row g-4 py-5 rows-cols-1'>
-                                        <ReactMarkdown
-                                            children={course.lessons[clicked].content}
-                                            className='single-post'
-                                        />
-                                    </div>
-
-                                    {/* video area */}
-                                    <div className='row g-4 py-5 mb-3'>
-                                        {course.lessons[clicked].video &&
-                                            course.lessons[clicked].video.Location && (
-                                                <>
-                                                    {/* video col */}
-                                                    <div className='feature col'>
-                                                        <div className='feature-icon bg-primary bg-gradient'>
-                                                            <svg className='bi' width='1em' height='1em'>
-                                                            </svg>
-                                                        </div>
-                                                        <h2>Video</h2>
-                                                        <div className='player'
-                                                        >
-                                                            <ReactPlayer
-                                                                url={course.lessons[clicked].video.Location}
-                                                                width='auto'
-                                                                height='500px'
-                                                                controls
-                                                                onEnded={markComplete} // update lesson completed status on video complete
-                                                            />
-                                                        </div>
-                                                    </div>
-
-                                                    <div className='feature col'>
-                                                        <div className='feature-icon bg-primary bg-gradient'>
-                                                            <svg className='bi' width='1em' height='1em'>
-                                                            </svg>
-                                                        </div>
-                                                        <h2>Confused?</h2>
-                                                        <ul className='card-subtitle'>
-                                                            <li>Read over lesson once more</li>
-                                                            <li>Re-watch video (if there is one)</li>
-                                                            <li>Raise your hand!</li>
-                                                            <br/>
-                                                            <li>YOU CAN DO THIS!</li>
-                                                        </ul>
-                                                    </div>
-
-                                                </>
-                                            )}
-                                    </div>
-
-                                    {/* code editors */}
-                                    {course.lessons[clicked].html &&
-                                        course.lessons[clicked].css && course.lessons[clicked].css && (
-                                            <>
-                                                <PlaygroundFrontEnd
-                                                    htmlValue={course.lessons[clicked].content}
-                                                    cssValue={course.lessons[clicked].css}
-                                                    jsValue={course.lessons[clicked].javascript}
-                                                />
-                                            < />
                                         )}
 
-                                    {/* description col*/}
-                                    <Content className='rounded-3 mt-3'>
-                                        <div className='container-fluid px-4 py-5 mb-5'>
-                                            <h2 className='lead title-large'>Are you stuck or getting
-                                                frustrated?</h2>
-                                            <p className='text-white text-center lead fs-4'>Pause. Breathe. Try
-                                                again.</p>
-                                            <p className='text-white text-center lead fs-5'>Raise your hand and ask
-                                                for
-                                                help! <br/>
-                                                We are all here to help, learn, and grow together.</p>
+                                        {/* course description*/}
+                                        <div className='row g-4 py-5 rows-cols-1'>
+                                            <ReactMarkdown
+                                                children={course.lessons[clicked].content}
+                                                className='single-post'
+                                            />
                                         </div>
-                                    </Content>
 
-                                </div>
-                            </Content>
+                                        {/* video area */}
+                                        <div className='row g-4 py-5 mb-3'>
+                                            {course.lessons[clicked].video &&
+                                                course.lessons[clicked].video.Location && (
+                                                    <>
+                                                        {/* video col */}
+                                                        <div className='feature col'>
+                                                            <div className='feature-icon bg-primary bg-gradient'>
+                                                                <svg className='bi' width='1em' height='1em'>
+                                                                </svg>
+                                                            </div>
+                                                            <h2>Video</h2>
+                                                            <div className='player'
+                                                            >
+                                                                <ReactPlayer
+                                                                    url={course.lessons[clicked].video.Location}
+                                                                    width='auto'
+                                                                    height='500px'
+                                                                    controls
+                                                                    onEnded={markComplete} // update lesson completed status on video complete
+                                                                />
+                                                            </div>
+                                                        </div>
+
+                                                        <div className='feature col'>
+                                                            <div className='feature-icon bg-primary bg-gradient'>
+                                                                <svg className='bi' width='1em' height='1em'>
+                                                                </svg>
+                                                            </div>
+                                                            <h2>Confused?</h2>
+                                                            <ul className='card-subtitle'>
+                                                                <li>Read over lesson once more</li>
+                                                                <li>Re-watch video (if there is one)</li>
+                                                                <li>Raise your hand!</li>
+                                                                <br/>
+                                                                <li>YOU CAN DO THIS!</li>
+                                                            </ul>
+                                                        </div>
+
+                                                    </>
+                                                )}
+                                        </div>
+
+                                        {/* code editors */}
+                                        {course.lessons[clicked].html &&
+                                            course.lessons[clicked].css && course.lessons[clicked].css && (
+                                                <>
+                                                    <PlaygroundFrontEnd
+                                                        htmlValue={course.lessons[clicked].content}
+                                                        cssValue={course.lessons[clicked].css}
+                                                        jsValue={course.lessons[clicked].javascript}
+                                                    />
+                                                < />
+                                            )}
+
+                                        {/* description col*/}
+                                        <Content className='rounded-3 mt-3'>
+                                            <div className='container-fluid px-4 py-5 mb-5'>
+                                                <h2 className='lead title-large'>Are you stuck or getting
+                                                    frustrated?</h2>
+                                                <p className='text-white text-center lead fs-4'>Pause. Breathe. Try
+                                                    again.</p>
+                                                <p className='text-white text-center lead fs-5'>Raise your hand and ask
+                                                    for help! <br/>
+                                                    We are all here to help, learn, and grow together.</p>
+                                            </div>
+                                        </Content>
+
+                                    </div>
+                                </Content>
 
 
                             </>
