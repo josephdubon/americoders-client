0d5bf286672bb19809afba6e5b17a808deffccf2
Joseph Dubon <josephdubon@pm.me>
2022-07-14 12:42:58 -0700

    updated component to match up with the course lessons page, matched up layout and styles

--- components/cards/SingleCourseHero.js
+++ components/cards/SingleCourseHero.js
@@ -1,7 +1,11 @@
-import {Badge, Button, Col, Image} from 'antd'
+import {Badge, Button, Divider, Image, Layout} from 'antd'
 import {currencyFormatter} from '../../utils/helpers'
 import ReactPlayer from 'react-player'
 import {LoadingOutlined, SafetyOutlined} from '@ant-design/icons'
+import Moment from 'moment'
+
+const {Content} = Layout
+
 
 const SingleCourseHero = ({
                               course,
@@ -31,26 +35,17 @@ const SingleCourseHero = ({
     } = course
 
     return (<>
-        <main>
-            <section className='py-5 text-center container'>
-                <div className='row py-lg-5'>
-                    <div className='col-lg-6 col-md-8 mx-auto'>
-                        <h1 className='fw-light'>{name}</h1>
-
-                    </div>
-                </div>
-            </section>
-
-            <div className='album py-5 bg-light'>
-                <div className='container'>
-                    <div className='row gap-3'>
-                        <div className='row row-cols-1 row-cols-md-2 g-3'>
-                            <Col>
-                                {/* title */}
-                                <h1 className='font-weight-bold'>{name}</h1>
-
-                                {/* description */}
-                                <p className='lead'>{description && description.substring(0, 160)}...</p>
+        {/* hero section */}
+        <Content className='bg-light'>
+            <Divider children='Welcome Back, Americoder!' className='mt-0 pt-3'/>
+            <div className='container col-xxl-12 px-4 py-5'>
+                <div
+                    className='row align-items-center justify-content-center g-5 row-cols-sm-1 row-cols-md-2 row-cols-lg-3'>
+                    <div>
+                        {course ?
+                            (<>
+                                <h1 className='display-6 fw-bold lh-1 mb-3 '>{name}</h1>
+                                <Divider/>
 
                                 {/* category */}
                                 <Badge
@@ -61,11 +56,16 @@ const SingleCourseHero = ({
                                     }}
                                 />
 
-                                {/* author */}
-                                <p>Created by {instructor.name}</p>
+                                {/* description*/}
+                                <p className='lead text-muted'>{description && description.substring(0, 160)}</p>
 
-                                {/* updated at */}
-                                <p>Last update: {new Date(updatedAt).toLocaleDateString()}</p>
+                                {/* author */}
+                                <p className='text-muted'><strong>Created by: </strong> {instructor.name}</p>
+
+                                {/* last update */}
+                                <p className='text-muted'><strong>Last
+                                    Update: </strong>{Moment(course && course.updatedAt).format('LL')}
+                                </p>
 
                                 {/* price */}
                                 <h4 className='text-black'>
@@ -76,66 +76,73 @@ const SingleCourseHero = ({
                                         }) : 'Free'
                                     }
                                 </h4>
-                            </Col>
-                            <Col>
-                                {/* show video preview or course image */}
-                                {lessons[0].video && lessons[0].video.Location ? (
-                                    <div
-                                        onClick={() => {
-                                            setPreview(lessons[0].video.Location)
-                                            setShowModal(!showModal) // toggle modal
-                                        }}
-                                    >
-                                        <ReactPlayer
-                                            className='react-player-div'
-                                            url={lessons[0].video.Location}
-                                            light={image.Location}
-                                            width='100%'
-                                            height='350px'
-                                            controls
-                                        />
-                                    </div>
-                                ) : (<>
-                                    {image && image.Location ?
-                                        <img src={image.Location}
-                                             alt={name}
-                                             style={{height: '240px', objectFit: 'cover'}}
-                                             className='p-1'
-                                        /> : <img src='/images/americoders-course.png'
-                                                  alt={name}
-                                                  style={{height: '240px', objectFit: 'cover'}}
-                                                  className='p-1'
-                                        />}
-                                </>)}
-                                {/* enroll button */}
-                                {loading ? <div className='d-flex justify-content-center'>
-                                    <LoadingOutlined className='h1 text-danger'/>
-                                </div> : (
-                                    <Button
-                                        className='mb-3 mt-3'
-                                        type='danger'
-                                        block
-                                        shape='round'
-                                        icon={<SafetyOutlined/>}
-                                        size='large'
-                                        disabled={loading}
-                                        onClick={paid ? handlePaidEnrollment : handleFreeEnrollment}
-                                    >
-                                        {user
-                                            ? enrolled.status
-                                                ? 'Go to course'
-                                                : 'Enroll'
-                                            : 'Login to enroll'}
-                                    </Button>
-                                )}
-                            </Col>
-                        </div>
+                            </>)
+                            :
+                            (<>
+                                <p className='display-6 fw-bold lh-1 mb-3'>Welcome back!!</p>
+                            </>)}
+                    </div>
+
+                    {/* course image and video */}
+                    <div className='d-flex flex-column'>
+                        {lessons[0].video && lessons[0].video.Location ? (
+                            <div
+                                onClick={() => {
+                                    setPreview(lessons[0].video.Location)
+                                    setShowModal(!showModal) // toggle modal
+                                }}
+                            >
+                                <ReactPlayer
+                                    className='react-player-div'
+                                    url={lessons[0].video.Location}
+                                    light={image.Location}
+                                    width='100%'
+                                    height='350px'
+                                    controls
+                                />
+                            </div>
+                        ) : (<>
+                            <div className='image-course'>
+                                {image && image.Location ?
+                                    <Image src={image.Location}
+                                           alt={name}
+                                           className='p-1 squareFrame'
+                                           preview={false}
+                                    /> : <Image src='/images/americoders-course.png'
+                                                alt={name}
+                                                className='p-1 squareFrame'
+                                    />
+                                }
+                            </div>
+                        </>)
+                        }
+
+                        {/* enroll button */}
+                        {loading ? <div className='d-flex justify-content-center'>
+                            <LoadingOutlined className='h1 text-danger'/>
+                        </div> : (
+                            <Button
+                                className='mb-3 mt-3'
+                                type='danger'
+                                block
+                                shape='round'
+                                icon={<SafetyOutlined/>}
+                                size='large'
+                                disabled={loading}
+                                onClick={paid ? handlePaidEnrollment : handleFreeEnrollment}
+                            >
+                                {user
+                                    ? enrolled.status
+                                        ? 'Go to course'
+                                        : 'Enroll'
+                                    : 'Login to enroll'}
+                            </Button>
+                        )}
                     </div>
 
                 </div>
             </div>
-
-        </main>
+        </Content>
     </>)
 }
 
