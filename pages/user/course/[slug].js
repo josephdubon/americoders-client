175cffc43635e5f26a5eb43a9596ae30db7bedb1
Joseph Dubon <josephdubon@pm.me>
2022-07-13 20:15:48 -0700

    removed cta and readded as its own component

--- pages/user/course/[slug].js
+++ pages/user/course/[slug].js
@@ -266,19 +266,9 @@ const SingleCourse = () => {
                                                 < />
                                             )}
 
-                                        {/* description col*/}
-                                        <Content className='rounded-3 mt-3'>
-                                            <div className='container-fluid px-4 py-5 mb-5'>
-                                                <h2 className='lead title-large'>Are you stuck or getting
-                                                    frustrated?</h2>
-                                                <p className='text-white text-center lead fs-4'>Pause. Breathe. Try
-                                                    again.</p>
-                                                <p className='text-white text-center lead fs-5'>Raise your hand and ask
-                                                    for help! <br/>
-                                                    We are all here to help, learn, and grow together.</p>
-                                            </div>
-                                        </Content>
-
+                                        <div>
+                                            <AskForHelp/>
+                                        </div>
                                     </div>
                                 </Content>
 
