18c9a147d1b2d9a2e3f5231d4e58670620b49aba
Joseph Dubon <josephdubon@pm.me>
2022-07-17 18:59:13 -0700

    commented out course card

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import {Col, Divider, Layout, Row} from 'antd'
 import Link from 'next/link'
 import Image from 'next/image'
 import MailingListForm from '../components/forms/MailingListForm'
-import CourseCard from '../components/cards/CourseCard'
+// import CourseCard from '../components/cards/CourseCard'
 
 const {Content} = Layout
 
