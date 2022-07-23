357265b07504a2f1f36fe3cafe2bb9a1a644a4bb
Joseph Dubon <josephdubon@pm.me>
2022-07-22 18:54:41 -0700

    updated padding and margin

--- components/structure/FeatureSectionRight/FeatureSectionRight.js
+++ components/structure/FeatureSectionRight/FeatureSectionRight.js
@@ -5,14 +5,12 @@ export default function FeatureSectionLeft({featuredImage, featuredTitle, featur
         <Row
             align={'middle'}
             justify={'center'}
-            className='container'
-
+            className='container py-5'
         >
             {/* content */}
             <Col
                 xs={24}
                 lg={14}
-                className={'px-3 py-5'}
             >
                 <h2 className='py-3 text-uppercase text-white text-center'>{featuredTitle}</h2>
                 {featuredContent}
@@ -26,7 +24,7 @@ export default function FeatureSectionLeft({featuredImage, featuredTitle, featur
                 <Image
                     src={`${featuredImage}`}
                     preview={false}
-                    className='rounded-3'
+                    className='rounded-3 bg-body p-3'
                 />
             </Col>
         </Row>
