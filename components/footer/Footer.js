fe1dd9ac808986408c66d6d7601ad1b1be8939ea
Joseph Dubon <josephdubon@pm.me>
2022-08-29 12:59:58 -0700

    removed helper code and updated left side footer nav lnks

--- components/footer/Footer.js
+++ components/footer/Footer.js
@@ -1,91 +1,66 @@
 /*eslint-disable*/
-import React from "react";
+import React from 'react'
 // nodejs library to set properties for components
-import PropTypes from "prop-types";
+import PropTypes from 'prop-types'
 // nodejs library that concatenates classes
-import classNames from "classnames";
+import classNames from 'classnames'
 // material-ui core components
-import { List, ListItem } from "@material-ui/core";
-import { makeStyles } from "@material-ui/core/styles";
+import { List } from '@material-ui/core'
+import { makeStyles } from '@material-ui/core/styles'
 
 // @material-ui/icons
-import Favorite from "@material-ui/icons/Favorite";
+import styles from '/styles/jss/nextjs-material-kit/components/footerStyle.js'
+import Link from 'next/link'
+import { ListItem } from '@mui/material'
 
-import styles from "/styles/jss/nextjs-material-kit/components/footerStyle.js";
+const useStyles = makeStyles(styles)
 
-const useStyles = makeStyles(styles);
-
-export default function Footer(props) {
-  const classes = useStyles();
-  const { whiteFont } = props;
+export default function Footer (props) {
+  const classes = useStyles()
+  const { whiteFont } = props
   const footerClasses = classNames({
     [classes.footer]: true,
-    [classes.footerWhiteFont]: whiteFont
-  });
+    [classes.footerWhiteFont]: whiteFont,
+  })
   const aClasses = classNames({
     [classes.a]: true,
-    [classes.footerWhiteFont]: whiteFont
-  });
+    [classes.footerWhiteFont]: whiteFont,
+  })
   return (
     <footer className={footerClasses}>
       <div className={classes.container}>
         <div className={classes.left}>
           <List className={classes.list}>
+
+            {/* add left side links here */}
             <ListItem className={classes.inlineBlock}>
-              <a
-                href="https://www.creative-tim.com/?ref=njsmk-footer"
-                className={classes.block}
-                target="_blank"
-              >
-                Creative Tim
-              </a>
-            </ListItem>
-            <ListItem className={classes.inlineBlock}>
-              <a
-                href="https://www.creative-tim.com/presentation?ref=njsmk-footer"
-                className={classes.block}
-                target="_blank"
-              >
-                About us
-              </a>
-            </ListItem>
-            <ListItem className={classes.inlineBlock}>
-              <a
-                href="http://blog.creative-tim.com/?ref=njsmk-footer"
-                className={classes.block}
-                target="_blank"
-              >
-                Blog
-              </a>
-            </ListItem>
-            <ListItem className={classes.inlineBlock}>
-              <a
-                href="https://www.creative-tim.com/license?ref=njsmk-footer"
-                className={classes.block}
-                target="_blank"
-              >
-                Licenses
-              </a>
+              <Link href={'/landing'} as={'/home'}>
+                <a
+                  className={classes.block}
+                >
+                  Home
+                </a>
+              </Link>
             </ListItem>
           </List>
         </div>
+
+        {/* right side credit line */}
         <div className={classes.right}>
-          &copy; {1900 + new Date().getYear()} , made with{" "}
-          <Favorite className={classes.icon} /> by{" "}
-          <a
-            href="https://www.creative-tim.com?ref=njsmk-footer"
-            className={aClasses}
-            target="_blank"
-          >
-            Creative Tim
-          </a>{" "}
-          for a better web.
+          &copy; {1900 + new Date().getYear()}
+          {' '}
+          <Link href={'/landing'} as={'/home'}>
+            <a className={aClasses}>
+              AMERICODERS
+            </a>
+          </Link>
+
         </div>
       </div>
     </footer>
-  );
+  )
 }
 
 Footer.propTypes = {
-  whiteFont: PropTypes.bool
-};
+  whiteFont: PropTypes.bool,
+}
