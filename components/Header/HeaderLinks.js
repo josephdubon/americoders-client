b313cbd61b9563ad328a182c1edf102223bf9d80
Joseph Dubon <josephdubon@pm.me>
2022-08-29 11:56:58 -0700

    added new components for material ui

---
+++ components/Header/HeaderLinks.js
@@ -0,0 +1,181 @@
+/*eslint-disable*/
+import React, { useContext, useEffect, useState } from 'react'
+
+// @material-ui/core components
+import { makeStyles } from '@material-ui/core/styles'
+import List from '@material-ui/core/List'
+import ListItem from '@material-ui/core/ListItem'
+import Tooltip from '@material-ui/core/Tooltip'
+import Icon from '@material-ui/core/Icon'
+
+// @material-ui/icons
+import { HowToReg } from '@material-ui/icons'
+
+// core components
+import Button from '../../components/CustomButtons/Button.js'
+
+import styles
+  from '../../styles/jss/nextjs-material-kit/components/headerLinksStyle.js'
+import { Context } from '../../context'
+import useRouter from 'next/router'
+import axios from 'axios'
+import { toast } from 'react-toastify'
+
+const useStyles = makeStyles(styles)
+
+export default function HeaderLinks (props) {
+  const classes = useStyles()
+
+  // set state for current page/link
+  const [currentPage, setCurrentPage] = useState('')
+  const isServer = () => typeof window !== 'undefined'
+  const { state, dispatch } = useContext(Context)
+  const { user } = state
+
+  const router = useRouter
+
+  // update state on page change
+  useEffect(() => {
+    // process.browser is depreciated use typeof instead
+    isServer() && setCurrentPage(window.location.pathname)
+  }, [isServer() && window.location.pathname])
+
+  // logout logic
+  const logout = async () => {
+    dispatch({
+      type: 'LOGOUT',
+    })
+
+    // clear out local storage, context, and redirect
+    window.localStorage.removeItem('user')
+
+    const { data } = await axios.get('/api/logout')
+
+    // notification config
+    toast(data.message, {
+      position: 'top-center',
+      autoClose: 5000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+    })
+
+    await router.push('/login')
+  }
+
+  return (
+    <List className={classes.list}>
+
+      {/* start nav items */}
+
+      {/*<ListItem className={classes.listItem}>*/}
+      {/*  <CustomDropdown*/}
+      {/*    noLiPadding*/}
+      {/*    navDropdown*/}
+      {/*    buttonText="Components"*/}
+      {/*    buttonProps={{*/}
+      {/*      className: classes.navLink,*/}
+      {/*      color: "transparent"*/}
+      {/*    }}*/}
+      {/*    buttonIcon={Apps}*/}
+      {/*    dropdownList={[*/}
+      {/*      <Link href="/components">*/}
+      {/*        <a className={classes.dropdownLink}>All components</a>*/}
+      {/*      </Link>,*/}
+      {/*      <a*/}
+      {/*        href="https://creativetimofficial.github.io/nextjs-material-kit/#/documentation?ref=njsmk-navbar"*/}
+      {/*        target="_blank"*/}
+      {/*        className={classes.dropdownLink}*/}
+      {/*      >*/}
+      {/*        Documentation*/}
+      {/*      </a>*/}
+      {/*    ]}*/}
+      {/*  />*/}
+      {/*</ListItem>*/}
+
+      {/* login */}
+      <ListItem className={classes.listItem}>
+        <Button
+          href="https://www.creative-tim.com/product/nextjs-material-kit-pro?ref=njsmk-navbar"
+          color="transparent"
+          target="_blank"
+          className={classes.navLink}
+        >
+          <Icon className={classes.icons}>login</Icon> Login
+        </Button>
+      </ListItem>
+
+      {/* signup */}
+      <ListItem className={classes.listItem}>
+        <Button
+          href="https://www.creative-tim.com/product/nextjs-material-kit?ref=njsmk-navbar"
+          color="transparent"
+          target="_blank"
+          className={classes.navLink}
+        >
+          <HowToReg
+            className={classes.icons}/> Signup
+        </Button>
+      </ListItem>
+      <ListItem className={classes.listItem}>
+        {/*<Tooltip title="Delete">
+          <IconButton aria-label="Delete">
+            <DeleteIcon />
+          </IconButton>
+        </Tooltip>*/}
+        <Tooltip
+          id="instagram-twitter"
+          title="Follow us on twitter"
+          placement={'top'}
+          classes={{ tooltip: classes.tooltip }}
+        >
+          <Button
+            href="https://twitter.com/CreativeTim?ref=creativetim"
+            target="_blank"
+            color="transparent"
+            className={classes.navLink}
+          >
+            <i className={classes.socialIcons + ' fab fa-twitter'}/>
+          </Button>
+        </Tooltip>
+      </ListItem>
+      <ListItem className={classes.listItem}>
+        <Tooltip
+          id="instagram-facebook"
+          title="Follow us on facebook"
+          placement={'top'}
+          classes={{ tooltip: classes.tooltip }}
+        >
+          <Button
+            color="transparent"
+            href="https://www.facebook.com/CreativeTim?ref=creativetim"
+            target="_blank"
+            className={classes.navLink}
+          >
+            <i className={classes.socialIcons + ' fab fa-facebook'}/>
+          </Button>
+        </Tooltip>
+      </ListItem>
+      <ListItem className={classes.listItem}>
+        <Tooltip
+          id="instagram-tooltip"
+          title="Follow us on instagram"
+          placement={'top'}
+          classes={{ tooltip: classes.tooltip }}
+        >
+          <Button
+            color="transparent"
+            href="https://www.instagram.com/CreativeTimOfficial?ref=creativetim"
+            target="_blank"
+            className={classes.navLink}
+          >
+            <i className={classes.socialIcons + ' fab fa-instagram'}/>
+          </Button>
+        </Tooltip>
+      </ListItem>
+      {/* end nav items */}
+    </List>
+  )
+}
--- pages/login.js
+++ pages/login.js
@@ -1,59 +1,152 @@
-import Link from 'next/link'
-import {Image, Layout} from 'antd'
-import LoginForm from '../components/forms/LoginForm'
+import React from 'react'
+// @material-ui/core components
+import { makeStyles } from '@material-ui/core/styles'
+import InputAdornment from '@material-ui/core/InputAdornment'
+import Icon from '@material-ui/core/Icon'
+// @material-ui/icons
+import Email from '@material-ui/icons/Email'
+import People from '@material-ui/icons/People'
+// core components
+import Header from '../components/Header/Header.js'
+import HeaderLinks from '../components/Header/HeaderLinks.js'
+import Footer from '../components/Footer/Footer.js'
+import GridContainer from '../components/Grid/GridContainer.js'
+import GridItem from '../components/Grid/GridItem.js'
+import Button from '../components/CustomButtons/Button.js'
+import Card from '../components/Card/Card.js'
+import CardBody from '../components/Card/CardBody.js'
+import CardHeader from '../components/Card/CardHeader.js'
+import CardFooter from '../components/Card/CardFooter.js'
+import CustomInput from '../components/CustomInput/CustomInput.js'
 
-const {Content} = Layout
+import styles from '../styles/jss/nextjs-material-kit/pages/loginPage.js'
 
-const Login = () => {
+const useStyles = makeStyles(styles)
 
-    return (<>
-        {/* hero section */}
-        <Content className='bg-light'>
-            <div className='container col-xxl-8 px-4 py-5'>
-                <div className='row flex-lg-row-reverse align-items-center justify-content-center g-5 py-5'>
-                    <div className='col-10 col-sm-8 col-lg-6'>
-                        <Image
-                            src='/images/branding/misc/americoders-student-female-laptop-fun.jpg'
-                            alt='Americoders'
-                            loading='lazy'
-                            preview={false}
-                        />
-                        <p className=' text-muted mt-2'>
-                            Thank you for your patience as we build out our system. We are currently in development of
-                            our courses/workshops. Please keep checking back for new content!
-                        </p>
+export default function LoginPage (props) {
+  const [cardAnimaton, setCardAnimation] = React.useState('cardHidden')
+  setTimeout(function () {
+    setCardAnimation('')
+  }, 700)
+  const classes = useStyles()
+  const { ...rest } = props
+  return (
+    <div>
+      <Header
+        absolute
+        color="transparent"
+        brand="Americoders"
+        rightLinks={<HeaderLinks/>}
+        {...rest}
+      />
+      <div
+        className={classes.pageHeader}
+        style={{
+          backgroundImage: 'url(\'/img/bg7.jpg\')',
+          backgroundSize: 'cover',
+          backgroundPosition: 'top center',
+        }}
+      >
+        <div className={classes.container}>
+          <GridContainer justify="center">
+            <GridItem xs={12} sm={6} md={4}>
+              <Card className={classes[cardAnimaton]}>
+                <form className={classes.form}>
+                  <CardHeader color="primary" className={classes.cardHeader}>
+                    <h4>Login</h4>
+                    <div className={classes.socialLine}>
+                      <Button
+                        justIcon
+                        href="#pablo"
+                        target="_blank"
+                        color="transparent"
+                        onClick={(e) => e.preventDefault()}
+                      >
+                        <i className={'fab fa-twitter'}/>
+                      </Button>
+                      <Button
+                        justIcon
+                        href="#pablo"
+                        target="_blank"
+                        color="transparent"
+                        onClick={(e) => e.preventDefault()}
+                      >
+                        <i className={'fab fa-facebook'}/>
+                      </Button>
+                      <Button
+                        justIcon
+                        href="#pablo"
+                        target="_blank"
+                        color="transparent"
+                        onClick={(e) => e.preventDefault()}
+                      >
+                        <i className={'fab fa-google-plus-g'}/>
+                      </Button>
                     </div>
-                    <div className='col-lg-6'>
-                        <h1 className='display-5 fw-bold lh-1 mb-3'>Login</h1>
-                        <p className='lead text-muted'>
-                            Welcome back, <strong className='text-primary fw-bold'>Americoder</strong>!
-                        </p>
-                        <p className=' text-muted mt-2'>
-                            Thanks for visiting again! Always remember to reach out if you have any questions or
-                            comments.
-                        </p>
-
-                        {/* Login Form */}
-                        <div className='col-md-12 offset-md-12 pb-5'>
-
-                            {/* login form */}
-                            <LoginForm/>
-                            <br/>
-
-                            {/*  register */}
-                            <p className='form-text text-center p3'>
-                                Want to sign-up? <Link href={'/register'}><a>Register</a></Link>
-                                <br/>
-                                {/* reset password */}
-                                <Link href={'/forgot-password'}><a className='text-danger'>Forgot password?</a></Link>
-                            </p>
-                        </div>
-
-                    </div>
-                </div>
-            </div>
-        </Content>
-    </>)
+                  </CardHeader>
+                  <p className={classes.divider}>Or Be Classical</p>
+                  <CardBody>
+                    <CustomInput
+                      labelText="First Name..."
+                      id="first"
+                      formControlProps={{
+                        fullWidth: true,
+                      }}
+                      inputProps={{
+                        type: 'text',
+                        endAdornment: (
+                          <InputAdornment position="end">
+                            <People className={classes.inputIconsColor}/>
+                          </InputAdornment>
+                        ),
+                      }}
+                    />
+                    <CustomInput
+                      labelText="Email..."
+                      id="email"
+                      formControlProps={{
+                        fullWidth: true,
+                      }}
+                      inputProps={{
+                        type: 'email',
+                        endAdornment: (
+                          <InputAdornment position="end">
+                            <Email className={classes.inputIconsColor}/>
+                          </InputAdornment>
+                        ),
+                      }}
+                    />
+                    <CustomInput
+                      labelText="Password"
+                      id="pass"
+                      formControlProps={{
+                        fullWidth: true,
+                      }}
+                      inputProps={{
+                        type: 'password',
+                        endAdornment: (
+                          <InputAdornment position="end">
+                            <Icon className={classes.inputIconsColor}>
+                              lock_outline
+                            </Icon>
+                          </InputAdornment>
+                        ),
+                        autoComplete: 'off',
+                      }}
+                    />
+                  </CardBody>
+                  <CardFooter className={classes.cardFooter}>
+                    <Button simple color="primary" size="lg">
+                      Get started
+                    </Button>
+                  </CardFooter>
+                </form>
+              </Card>
+            </GridItem>
+          </GridContainer>
+        </div>
+        <Footer whiteFont/>
+      </div>
+    </div>
+  )
 }
-
-export default Login
