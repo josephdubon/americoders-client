62e06a2ec0785c1612978580301762556337cd8a
Joseph Dubon <josephdubon@pm.me>
2022-08-29 14:11:55 -0700

    updated frontend with material ui used new formatting styles

--- components/forms/LoginForm.js
+++ components/forms/LoginForm.js
@@ -34,57 +34,58 @@ const LoginForm = () => {
     dispatch,
   } = useContext(Context)
 
+  // router
+  const router = useRouter()
 
-    // router
-    const router = useRouter()
+  // condition redirect for logged-in user
+  useEffect(() => {
+    if (user !== null) router.push('/user')
+  }, [user])
 
-    // condition redirect for logged-in user
-    useEffect(() => {
-        if (user !== null) router.push('/user')
-    }, [user])
+  const handleSubmit = async (e) => {
+    // do not reload the page
+    e.preventDefault()
 
-    const handleSubmit = async (e) => {
-        // do not reload the page
-        e.preventDefault()
+    // send data to backend
+    try {
+      // activate load spinner
+      setLoading(true)
+      const { data } = await axios.post(`/api/login`, {
+        email, password,
+      })
 
-        // send data to backend
-        try {
-            // activate load spinner
-            setLoading(true)
-            const {data} = await axios.post(`/api/login`, {
-                email, password
-            })
+      // notification config
+      toast.success(
+        'Welcome to Americoders! What will you create to make the world a better place?',
+        {
+          position: 'top-center',
+          autoClose: 5000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+        })
 
-            // notification config
-            toast.success('Welcome to Americoders! What will you create to make the world a better place?', {
-                position: 'top-center',
-                autoClose: 5000,
-                hideProgressBar: false,
-                closeOnClick: true,
-                pauseOnHover: true,
-                draggable: true,
-                progress: undefined,
-            })
+      // deactivate load spinner
+      setLoading(false)
 
-            // deactivate load spinner
-            setLoading(false)
+      // console.log('LOGIN RESPONSE', data)
+      dispatch({
+        type: 'LOGIN',
+        payload: data,
+      })
 
-            // console.log('LOGIN RESPONSE', data)
-            dispatch({
-                type: 'LOGIN',
-                payload: data,
-            })
+      // save state in local storage
+      window.localStorage.setItem('user', JSON.stringify(data))
 
-            // save state in local storage
-            window.localStorage.setItem('user', JSON.stringify(data))
-
-            // clear fields and redirect
-            setEmail('')
-            setPassword('')
-            await router.push('user')
-        } catch (err) {
-            // deactivate load spinner
-            setLoading(false)
+      // clear fields and redirect
+      setEmail('')
+      setPassword('')
+      await router.push('user')
+    } catch (err) {
+      // deactivate load spinner
+      setLoading(false)
 
             toast.error(err.response.data, {
                 position: 'top-center',
@@ -98,36 +99,97 @@ const LoginForm = () => {
         }
     }
 
-    return (<>
-        <form onSubmit={handleSubmit}>
-            <input
-                type='email'
-                className='form-control mb-4 p-4'
-                value={email}
-                onChange={e => setEmail(e.target.value)}
-                placeholder='Enter email'
-                required
-            />
-            <input
-                type='password'
-                className='form-control mb-4 p-4'
-                value={password}
-                onChange={e => setPassword(e.target.value)}
-                placeholder='Enter password'
-                required
-            />
-            <div className='d-grid gap-2'>
-                <button
-                    type='submit'
-                    className='btn btn-primary'
-                    disabled={!email || !password || loading}
-                >
-                    {loading ? <SyncOutlined spin/> : 'Submit'}
-                </button>
-            </div>
-        </form>
+  return (<>
+    <form
+      className={classes.form}
+      onSubmit={handleSubmit}>
+      <CardHeader color="primary" className={classes.cardHeader}>
+        <h4>Login</h4>
 
-    </>)
+        {/* social login icons */}
+        {/*<div className={classes.socialLine}>*/}
+        {/*  <Button*/}
+        {/*    justIcon*/}
+        {/*    href="#TODO add a social login option"*/}
+        {/*    target="_blank"*/}
+        {/*    color="transparent"*/}
+        {/*    onClick={(e) => e.preventDefault()}*/}
+        {/*  >*/}
+        {/*    <i className={'fab fa-twitter'}/>*/}
+        {/*  </Button>*/}
+        {/*  <Button*/}
+        {/*    justIcon*/}
+        {/*    href="#TODO add a social login option"*/}
+        {/*    target="_blank"*/}
+        {/*    color="transparent"*/}
+        {/*    onClick={(e) => e.preventDefault()}*/}
+        {/*  >*/}
+        {/*    <i className={'fab fa-facebook'}/>*/}
+        {/*  </Button>*/}
+        {/*  <Button*/}
+        {/*    justIcon*/}
+        {/*    href="#TODO add a social login option"*/}
+        {/*    target="_blank"*/}
+        {/*    color="transparent"*/}
+        {/*    onClick={(e) => e.preventDefault()}*/}
+        {/*  >*/}
+        {/*    <i className={'fab fa-google-plus-g'}/>*/}
+        {/*  </Button>*/}
+        {/*</div>*/}
+      </CardHeader>
+
+      <p className={classes.divider}>Code to Live, Live to Code</p>
+      <CardBody>
+        <CustomInput
+          labelText="Email..."
+          id="email"
+          formControlProps={{
+            fullWidth: true,
+          }}
+          inputProps={{
+            type: 'email',
+            value: email,
+            onChange: e => setEmail(e.target.value),
+            endAdornment: (
+              <InputAdornment position="end">
+                <Email className={classes.inputIconsColor}/>
+              </InputAdornment>
+            ),
+          }}
+        />
+        <CustomInput
+          labelText="Password"
+          id="pass"
+          formControlProps={{
+            fullWidth: true,
+          }}
+          inputProps={{
+            type: 'password',
+            value: password,
+            onChange: e => setPassword(e.target.value),
+            endAdornment: (
+              <InputAdornment position="end">
+                <Icon className={classes.inputIconsColor}>
+                  lock_outline
+                </Icon>
+              </InputAdornment>
+            ),
+            autoComplete: 'off',
+          }}
+        />
+      </CardBody>
+      <CardFooter className={classes.cardFooter}>
+        <Button
+          type="submit"
+          color="primary"
+          size="lg"
+          disabled={!email || !password || loading}
+        >
+          Get started
+        </Button>
+      </CardFooter>
+    </form>
+  </>)
 }
 
 export default LoginForm
