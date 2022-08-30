c62810479e647564f2515349a59caac475e64fc3
Joseph Dubon <josephdubon@pm.me>
2022-08-29 17:54:29 -0700

    added a commented out loop for the courses published

--- pages/landing.js
+++ pages/landing.js
@@ -77,6 +77,17 @@ export default function LandingPage (props) {
       </Parallax>
       <div className={classNames(classes.main, classes.mainRaised)}>
         <div className={classes.container}>
+          {/*<p>start</p>*/}
+          {/*loop through the published courses*/}
+          {/*{props.courses.map((course) => (*/}
+          {/*    <Col key={course._id}>*/}
+          {/*      <CourseCard course={course}/>*/}
+          {/*    </Col>*/}
+          {/*  ),*/}
+          {/*)}*/}
+          {/*end loop*/}
+          {/*<p>end</p>*/}
+
           <ProductSection/>
           <TeamSection/>
           <WorkSection/>
