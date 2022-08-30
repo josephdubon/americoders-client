0463ec0a51763ae5f0f1a37640e87528dcf40aad
Joseph Dubon <josephdubon@pm.me>
2022-08-30 15:19:34 -0700

    added quote and description updated p color

--- styles/jss/nextjs-material-kit/pages/landingPageSections/productStyle.js
+++ styles/jss/nextjs-material-kit/pages/landingPageSections/productStyle.js
@@ -1,20 +1,40 @@
-import { title } from "/styles/jss/nextjs-material-kit.js";
+import { title } from '../../../nextjs-material-kit'
 
 const productStyle = {
   section: {
-    padding: "70px 0",
-    textAlign: "center"
+    padding: '70px 0',
+    textAlign: 'center',
   },
   title: {
     ...title,
-    marginBottom: "1rem",
-    marginTop: "30px",
-    minHeight: "32px",
-    textDecoration: "none"
+    marginBottom: '1rem',
+    marginTop: '30px',
+    minHeight: '32px',
+    textDecoration: 'none',
+  },
+  subtitle: {
+    ...title,
+    fontSize: '1.313rem',
   },
   description: {
-    color: "#999"
-  }
-};
+    color: '#999',
+    fontSize: '1.1rem',
+    textAlign: 'left',
+    margin: '.5rem 1rem',
+  },
+  quote: {
+    color: '#4b4b4b',
+    fontSize: '.8rem',
+    fontWeight: '400',
+    textAlign: 'center',
+    margin: '.5rem 4rem',
+  },
+  imageBox: {
+    minHeight: '320px',
+  },
+  image: {
+    borderRadius: '2.5rem',
+  },
+}
 
-export default productStyle;
+export default productStyle
