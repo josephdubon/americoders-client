e502d8435d63bfcf8fb5cbc8a7bca0d27cb8ca85
Joseph Dubon <josephdubon@pm.me>
2022-08-29 17:56:05 -0700

    nuked helper code and added americoders data

--- pages/profile.js
+++ pages/profile.js
@@ -1,12 +1,9 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 // nodejs library that concatenates classes
 import classNames from 'classnames'
 // @material-ui/core components
 import { makeStyles } from '@material-ui/core/styles'
 // @material-ui/icons
-import Camera from '@material-ui/icons/Camera'
-import Palette from '@material-ui/icons/Palette'
-import Favorite from '@material-ui/icons/Favorite'
 // core components
 import Header from '../components/Header/Header.js'
 import Footer from '../components/Footer/Footer.js'
@@ -17,21 +14,57 @@ import HeaderLinks from '../components/Header/HeaderLinks.js'
 import NavPills from '../components/NavPills/NavPills.js'
 import Parallax from '../components/Parallax/Parallax.js'
 
+import { Context } from '../context'
+
 import styles from '../styles/jss/nextjs-material-kit/pages/profilePage.js'
+import axios from 'axios'
+import Moment from 'moment/moment'
+import { School } from '@material-ui/icons'
+import UserRoute from '../components/routes/UserRoute'
+import { PageHead } from '../components/PageHead/PageHead'
+import Link from 'next/link'
 
 const useStyles = makeStyles(styles)
 
 export default function ProfilePage (props) {
+  // state
+  const [courses, setCourses] = useState([])
+  const [loading, setLoading] = useState(false)
+
+  // get user
+  const {
+    state: { user },
+  } = useContext(Context)
+
+  useEffect(() => {
+    loadCourses()
+  }, [])
+
+  const loadCourses = async () => {
+    // update state
+    setLoading(true)
+
+    // get data
+    const { data } = await axios.get('/api/user-courses')
+
+    // update state
+    setCourses(data)
+    setLoading(false)
+  }
+
   const classes = useStyles()
   const { ...rest } = props
   const imageClasses = classNames(
     classes.imgRaised,
-    classes.imgRoundedCircle,
     classes.imgFluid,
   )
   const navImageClasses = classNames(classes.imgRounded, classes.imgGallery)
   return (
-    <div>
+    <UserRoute>
+
+      <PageHead title={`Welcome back, ${user && user.firstName} ${user &&
+      user.lastName}!`}/>
+
       <Header
         color="transparent"
         brand="Americoders"
@@ -52,14 +85,38 @@ export default function ProfilePage (props) {
                 <div className={classes.profile}>
                   <div>
                     <img
-                      src="/img/faces/christian.jpg"
+                      src={user && user.picture}
                       alt="..."
                       className={imageClasses}
+                      style={{ marginBottom: '25px' }}
                     />
                   </div>
+
                   <div className={classes.name}>
-                    <h3 className={classes.title}>Christian Louboutin</h3>
-                    <h6>DESIGNER</h6>
+                    {/* name */}
+                    <h3 className={classes.title}>
+                      {`${user && user.firstName} ${user && user.lastName}`}
+                    </h3>
+                    <br/>
+                    <Link href={'/update-user/'}>
+                      <a style={{
+                        fontSize: '12px',
+                        textTransform: 'uppercase',
+                      }}>Confirm or Update Details</a>
+                    </Link>
+                    <h5>{user && user.email}</h5>
+
+                    {/* user details */}
+                    <h6>Role: {user && user.role.join(', ')}</h6>
+                    <h6>Enrolled: {courses && courses.length + ' '}
+                      {user && user.courses.length > 1 || 0
+                        ? 'Course'
+                        : 'Courses'}</h6>
+                    <h6>Member Since: {Moment(user && user.createdAt,
+                      'YYYYMMDD').
+                      fromNow()}</h6>
+
+                    {/* icons */}
                     <Button justIcon link className={classes.margin5}>
                       <i className={'fab fa-twitter'}/>
                     </Button>
@@ -69,16 +126,15 @@ export default function ProfilePage (props) {
                     <Button justIcon link className={classes.margin5}>
                       <i className={'fab fa-facebook'}/>
                     </Button>
+
                   </div>
                 </div>
               </GridItem>
             </GridContainer>
+
             <div className={classes.description}>
               <p>
-                An artist of considerable range, Chet Faker — the name taken by
-                Melbourne-raised, Brooklyn-based Nick Murphy — writes, performs
-                and records all of his own music, giving it a warm, intimate
-                feel with a solid groove structure.{' '}
+                {user && user.bio}
               </p>
             </div>
             <GridContainer justify="center">
@@ -88,111 +144,114 @@ export default function ProfilePage (props) {
                   color="primary"
                   tabs={[
                     {
-                      tabButton: 'Studio',
-                      tabIcon: Camera,
+                      tabButton: 'My Courses',
+                      tabIcon: School,
                       tabContent: (
                         <GridContainer justify="center">
-                          <GridItem xs={12} sm={12} md={4}>
-                            <img
-                              alt="..."
-                              src="/img/examples/studio-1.jpg"
-                              className={navImageClasses}
-                            />
-                            <img
-                              alt="..."
-                              src="/img/examples/studio-2.jpg"
-                              className={navImageClasses}
-                            />
-                          </GridItem>
-                          <GridItem xs={12} sm={12} md={4}>
-                            <img
-                              alt="..."
-                              src="/img/examples/studio-5.jpg"
-                              className={navImageClasses}
-                            />
-                            <img
-                              alt="..."
-                              src="/img/examples/studio-4.jpg"
-                              className={navImageClasses}
-                            />
-                          </GridItem>
+
+                          {/* list all courses */}
+                          {courses && courses.map(course => (
+                            <GridItem xs={12} sm={12} md={4} key={course._id}
+                                      className={classes.marginBottom}>
+                              <Link
+                                href={`/user/course/${course.slug}`}
+                              >
+                                <a>
+                                  <img
+                                    alt="..."
+                                    src={course.image
+                                      ? course.image.Location
+                                      : '/images/americoders-course.png'}
+                                    className={navImageClasses}
+                                  />
+                                  <h6 className={classes.marginBottom}>
+                                    {course.name}
+                                  </h6>
+                                </a>
+                              </Link>
+                              <p className={classes.description}>
+                                {/* show number of lessons in course */}
+                                {course.lessons.length} Lessons
+                              </p>
+                            </GridItem>
+                          ))}
                         </GridContainer>
                       ),
                     },
-                    {
-                      tabButton: 'Work',
-                      tabIcon: Palette,
-                      tabContent: (
-                        <GridContainer justify="center">
-                          <GridItem xs={12} sm={12} md={4}>
-                            <img
-                              alt="..."
-                              src="/img/examples/olu-eletu.jpg"
-                              className={navImageClasses}
-                            />
-                            <img
-                              alt="..."
-                              src="/img/examples/clem-onojeghuo.jpg"
-                              className={navImageClasses}
-                            />
-                            <img
-                              alt="..."
-                              src="/img/examples/cynthia-del-rio.jpg"
-                              className={navImageClasses}
-                            />
-                          </GridItem>
-                          <GridItem xs={12} sm={12} md={4}>
-                            <img
-                              alt="..."
-                              src="/img/examples/mariya-georgieva.jpg"
-                              className={navImageClasses}
-                            />
-                            <img
-                              alt="..."
-                              src="/img/examples/clem-onojegaw.jpg"
-                              className={navImageClasses}
-                            />
-                          </GridItem>
-                        </GridContainer>
-                      ),
-                    },
-                    {
-                      tabButton: 'Favorite',
-                      tabIcon: Favorite,
-                      tabContent: (
-                        <GridContainer justify="center">
-                          <GridItem xs={12} sm={12} md={4}>
-                            <img
-                              alt="..."
-                              src="/img/examples/mariya-georgieva.jpg"
-                              className={navImageClasses}
-                            />
-                            <img
-                              alt="..."
-                              src="/img/examples/studio-3.jpg"
-                              className={navImageClasses}
-                            />
-                          </GridItem>
-                          <GridItem xs={12} sm={12} md={4}>
-                            <img
-                              alt="..."
-                              src="/img/examples/clem-onojeghuo.jpg"
-                              className={navImageClasses}
-                            />
-                            <img
-                              alt="..."
-                              src="/img/examples/olu-eletu.jpg"
-                              className={navImageClasses}
-                            />
-                            <img
-                              alt="..."
-                              src="/img/examples/studio-1.jpg"
-                              className={navImageClasses}
-                            />
-                          </GridItem>
-                        </GridContainer>
-                      ),
-                    },
+                    // {
+                    //   tabButton: 'Work',
+                    //   tabIcon: Palette,
+                    //   tabContent: (
+                    //     <GridContainer justify="center">
+                    //       <GridItem xs={12} sm={12} md={4}>
+                    //         <img
+                    //           alt="..."
+                    //           src="/img/examples/olu-eletu.jpg"
+                    //           className={navImageClasses}
+                    //         />
+                    //         <img
+                    //           alt="..."
+                    //           src="/img/examples/clem-onojeghuo.jpg"
+                    //           className={navImageClasses}
+                    //         />
+                    //         <img
+                    //           alt="..."
+                    //           src="/img/examples/cynthia-del-rio.jpg"
+                    //           className={navImageClasses}
+                    //         />
+                    //       </GridItem>
+                    //       <GridItem xs={12} sm={12} md={4}>
+                    //         <img
+                    //           alt="..."
+                    //           src="/img/examples/mariya-georgieva.jpg"
+                    //           className={navImageClasses}
+                    //         />
+                    //         <img
+                    //           alt="..."
+                    //           src="/img/examples/clem-onojegaw.jpg"
+                    //           className={navImageClasses}
+                    //         />
+                    //       </GridItem>
+                    //     </GridContainer>
+                    //   ),
+                    // },
+                    // {
+                    //   tabButton: 'Favorite',
+                    //   tabIcon: Favorite,
+                    //   tabContent: (
+                    //     <GridContainer justify="center">
+                    //       <GridItem xs={12} sm={12} md={4}>
+                    //         <img
+                    //           alt="..."
+                    //           src="/img/examples/mariya-georgieva.jpg"
+                    //           className={navImageClasses}
+                    //         />
+                    //         <img
+                    //           alt="..."
+                    //           src="/img/examples/studio-3.jpg"
+                    //           className={navImageClasses}
+                    //         />
+                    //       </GridItem>
+                    //       <GridItem xs={12} sm={12} md={4}>
+                    //         <img
+                    //           alt="..."
+                    //           src="/img/examples/clem-onojeghuo.jpg"
+                    //           className={navImageClasses}
+                    //         />
+                    //         <img
+                    //           alt="..."
+                    //           src="/img/examples/olu-eletu.jpg"
+                    //           className={navImageClasses}
+                    //         />
+                    //         <img
+                    //           alt="..."
+                    //           src="/img/examples/studio-1.jpg"
+                    //           className={navImageClasses}
+                    //         />
+                    //       </GridItem>
+                    //     </GridContainer>
+                    //   ),
+                    // },
                   ]}
                 />
               </GridItem>
@@ -201,6 +260,6 @@ export default function ProfilePage (props) {
         </div>
       </div>
       <Footer/>
-    </div>
+    </UserRoute>
   )
 }
