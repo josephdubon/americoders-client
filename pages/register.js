1d486f2b5ab34f7d71b37953297bc11f281dfa27
Joseph Dubon <josephdubon@pm.me>
2022-08-29 17:57:01 -0700

    updated imports removed antd and bootstrap using material ui

--- pages/register.js
+++ pages/register.js
@@ -1,5 +1,17 @@
-import Link from 'next/link'
-import {Image, Layout} from 'antd'
+import React from 'react'
+// @material-ui/core components
+import { makeStyles } from '@material-ui/core/styles'
+// @material-ui/icons
+// core components
+import Header from '../components/Header/Header.js'
+import HeaderLinks from '../components/Header/HeaderLinks.js'
+import Footer from '../components/Footer/Footer.js'
+import GridContainer from '../components/Grid/GridContainer.js'
+import GridItem from '../components/Grid/GridItem.js'
+import Card from '../components/Card/Card.js'
+
+import styles from '../styles/jss/nextjs-material-kit/pages/loginPage.js'
+import LoginForm from '../components/forms/LoginForm'
 import StudentRegisterForm from '../components/forms/StudentRegisterForm'
 
 const {Content} = Layout
