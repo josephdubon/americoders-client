804a271d6dd4f5497290bbc1288c7a063bd77bd4
Joseph Dubon <josephdubon@pm.me>
2022-08-29 17:54:15 -0700

    removed ref from video

--- pages/landing.js
+++ pages/landing.js
@@ -64,7 +64,7 @@ export default function LandingPage (props) {
               <Button
                 color="danger"
                 size="lg"
-                href="https://www.youtube.com/watch?v=dQw4w9WgXcQ&ref=creativetim"
+                href="https://www.youtube.com/watch?v=dQw4w9WgXcQ&="
                 target="_blank"
                 rel="noopener noreferrer"
               >
