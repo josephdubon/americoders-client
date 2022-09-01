3fb5014e7664c410b62e4b1e345f10c0416a81e5
Joseph Dubon <josephdubon@pm.me>
2022-09-01 09:48:13 -0700

    removed unsupported command '/** @type {import('next').NextConfig} */  const nextConfig = {    reactStrictMode: true,  } '

--- next.config.js
+++ next.config.js
@@ -1,11 +1,5 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
-
 module.exports = {
-  nextConfig,
   images: {
-    domains: ['americodersbucket.s3.us-west-2.amazonaws.com'],
-  },
+    domains: ['americodersbucket.s3.us-west-2.amazonaws.com']
+  }
 }
