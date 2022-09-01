47b42d3817df0869884e59d912c98725cdeefa27
Joseph Dubon <josephdubon@pm.me>
2022-08-31 17:12:04 -0700

    proper formatting

--- components/routes/UserRoute.js
+++ components/routes/UserRoute.js
@@ -1,52 +1,53 @@
-import {useEffect, useState} from 'react'
+import { useEffect, useState } from 'react'
 import axios from 'axios'
-import {useRouter} from 'next/router'
-import {SyncOutlined} from '@ant-design/icons'
+import { useRouter } from 'next/router'
+import { SyncOutlined } from '@ant-design/icons'
 import UserNav from '../nav/UserNav'
 
-const UserRoute = ({children, showNav = true}) => {
-    // state
-    const [ok, setOk] = useState(false)
+const UserRoute = ({ children, showNav = true }) => {
+  // state
+  const [ok, setOk] = useState(false)
 
-    // router
-    const router = useRouter()
+  // router
+  const router = useRouter()
 
-    useEffect(() => {
-        fetchUser()
-    }, [])
+  useEffect(() => {
+    fetchUser()
+  }, [])
 
-    const fetchUser = async () => {
-        // try to get user match
-        try {
-            const {data} = await axios.get('/api/current-user')
-            if (data.ok) setOk(true) // if match found, set ok to true
-        } catch (err) {
-            console.log(err)
-            setOk(false) // if no match found, set ok to false
-            await router.push('/login') // redirect to login page
-        }
+  const fetchUser = async () => {
+    // try to get user match
+    try {
+      const { data } = await axios.get('/api/current-user')
+      if (data.ok) setOk(true) // if match found, set ok to true
+    } catch (err) {
+      console.log(err)
+      setOk(false) // if no match found, set ok to false
+      await router.push('/login') // redirect to login page
     }
+  }
 
-    return (<>
-        {!ok ? (
-            // if ok is false, show loading screen
-            <SyncOutlined spin className='d-flex justify-content-center display-1 text-primary p-5'
-            />
-        ) : (
-            // if ok is true, show child element
-            <>
-                <div className='container-fluid'>
-                    <div className='row'>
-                        <div className='col-md-2 mt-3'>
-                            {/* only show UserNav when showNav is true, hide on false */}
-                            {showNav && <UserNav/>}
-                        </div>
-                        <div className='col-md-10'>{children}</div>
-                    </div>
-                </div>
-            </>
-        )}
-    </>)
+  return (<>
+    {!ok ? (
+      // if ok is false, show loading screen
+      <SyncOutlined spin
+                    className="d-flex justify-content-center display-1 text-primary p-5"
+      />
+    ) : (
+      // if ok is true, show child element
+      <>
+        <div className="container-fluid">
+          <div className="row">
+            <div className="col-md-2 mt-3">
+              {/* only show UserNav when showNav is true, hide on false */}
+              {showNav && <UserNav/>}
+            </div>
+            <div className="col-md-10">{children}</div>
+          </div>
+        </div>
+      </>
+    )}
+  </>)
 }
 
 export default UserRoute
