88a98815769196fe0f836ff3194a160d79170afc
Joseph Dubon <josephdubon@pm.me>
2022-09-14 12:50:39 -0700

    readded components from old design I need these to rebuild some pages

---
+++ components/Footer/SiteFooter.js
@@ -0,0 +1,16 @@
+import Link from 'next/link'
+
+const SiteFooter = () => {
+    return (<>
+        <footer className='text-muted py-5'>
+            <div className='container'>
+                <p className='float-end mb-1'>
+                    <a href='#'>Back to top</a>
+                </p>
+                <p className='mb-1'>© {new Date().getFullYear()} <Link href='/'><a>Americoders</a></Link></p>
+            </div>
+        </footer>
+    </>)
+}
+
+export default SiteFooter
