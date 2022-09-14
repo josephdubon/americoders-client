592384bbc219e56f081f5946b8cf40f90699fa8c
Joseph Dubon <josephdubon@pm.me>
2022-09-14 12:53:25 -0700

    created new course hero component based off current sites design heavy use of material ui

---
+++ components/cards/SingleCourseHero.js
@@ -0,0 +1,136 @@
+import { currencyFormatter } from '../../utils/helpers'
+import { LoadingOutlined, SafetyOutlined } from '@ant-design/icons'
+import Header from '../Header/Header'
+import NavLogo from '../../public/images/logo/americoders-logo-simple_white.svg'
+import HeaderLinks from '../Header/HeaderLinks'
+import Parallax from '../Parallax/Parallax'
+import GridContainer from '../Grid/GridContainer'
+import GridItem from '../Grid/GridItem'
+import React, { useContext, useState } from 'react'
+import { makeStyles } from '@material-ui/core/styles'
+import styles from '../../styles/jss/americoders/pages/landingPage'
+import { Context } from '../../context'
+import { truncate } from 'lodash-es'
+import Badge from '../Badge/Badge.js'
+import Button from '../CustomButtons/Button'
+import moment from 'moment'
+
+const dashboardRoutes = []
+
+const useStyles = makeStyles(styles)
+
+const SingleCourseHero = (props) => {
+  // state
+  const [loading, setLoading] = useState(false)
+
+  const {
+    state: { user },
+  } = useContext(Context)
+
+  // destructure course items
+  const {
+    price,
+    paid,
+  } = props.course
+
+  const classes = useStyles()
+
+  const { ...rest } = props
+
+  const todayDate = moment().format('YYYY-MM-DD')
+  const disabledDate = moment('20221008').format('YYYY-MM-DD')
+
+  return (<>
+    {/* header section / nav */}
+    <Header
+      color="transparent"
+      routes={dashboardRoutes}
+      brand={NavLogo}
+      rightLinks={<HeaderLinks/>}
+      fixed
+      changeColorOnScroll={{
+        height: 400,
+        color: 'white',
+      }}
+      {...rest}
+    />
+
+    {/* hero section */}
+    {props.course ?
+      <Parallax filter responsive
+                image={props.course.image && props.course.image.Location}>
+        <div className={classes.container}>
+          <GridContainer
+            direction="row"
+            justifyContent="center"
+            alignItems="center"
+          >
+            <GridContainer direction="row"
+                           justifyContent="center"
+                           alignItems="center"
+            >
+              <GridItem xs={10} sm={10} md={12}>
+                {/* course name */}
+                <h1 className={classes.title}>{props.course && props.course.name}</h1>
+                <br/>
+                {/* category */}
+                <Badge color="success">
+                  {props.course.category}
+                </Badge>
+
+                {/* course description */}
+                <h4 className={classes.subtitle}>
+                  {truncate(props.course.description, { length: 240, omission: '...' })}
+                </h4>
+
+                {/* author */}
+                <p className="text-muted"><strong>Created by: </strong> {props.course.instructor.lastName}</p>
+
+                {/* price */}
+                <h4 className={classes.price}>
+                  {
+                    paid ? currencyFormatter({
+                      amount: price,
+                      currency: 'usd',
+                    }) : 'Free'
+                  }
+                </h4>
+
+                {/* enroll button */}
+                {loading ? <div className="d-flex justify-content-center">
+                  <LoadingOutlined className="h1 text-danger"/>
+                </div> : (
+                  <div>
+                    <Button
+                      color="danger"
+                      icon={<SafetyOutlined/>}
+                      size="lg"
+                      disabled={todayDate < disabledDate} // disable button if todays date is less than disabled date
+                      onClick={paid ? props.handlePaidEnrollment : props.handleFreeEnrollment}
+                    >
+                      {user
+                        ? props.enrolled.status
+                          ? 'Go to course'
+                          : 'Enroll'
+                        : 'Login to enroll'}
+                    </Button>
+                  </div>
+                )}
+
+              </GridItem>
+              <GridItem>
+              </GridItem>
+            </GridContainer>
+          </GridContainer>
+        </div>
+      </Parallax>
+      :
+      (<>
+          <p className="display-6 fw-bold lh-1 mb-3">Welcome back!!</p>
+        </>
+      )
+    }
+  </>)
+}
+
+export default SingleCourseHero
