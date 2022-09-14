3e8271ed0e7d2cfe6feed868ab626e537f9064e9
Joseph Dubon <josephdubon@pm.me>
2022-09-14 12:54:55 -0700

    created new course lists component this is used below the course hero section based off current sites design heavy use of material ui

---
+++ components/cards/SingleCourseLessons.js
@@ -0,0 +1,80 @@
+import Card from '../Card/Card'
+import { CardHeader, ListItem, ListItemIcon, ListItemText } from '@mui/material'
+import CardBody from '../Card/CardBody'
+import CardFooter from '../Card/CardFooter'
+import { cardTitle } from '../../styles/jss/americoders'
+import { makeStyles } from '@material-ui/core/styles'
+import { School } from '@material-ui/icons'
+import { truncate } from 'lodash-es'
+import Moment from 'moment/moment'
+import { List } from '@material-ui/core'
+
+const styles = {
+  cardTitle,
+  textCenter: {
+    textAlign: 'center'
+  },
+  textMuted: {
+    color: '#6c757d'
+  },
+  icon: {
+    color: '#999',
+    fontSize: '1.1rem',
+    textAlign: 'left',
+    textTransform: 'uppercase'
+  }
+}
+
+const useStyles = makeStyles(styles)
+
+const SingleCourseLessons = ({
+  lessons,
+  setPreview,
+  showModal,
+  setShowModal,
+  course,
+}) => {
+
+  // state
+  const classes = useStyles()
+  return (<>
+    <Card className={classes.textCenter}>
+      <CardHeader color="danger">Featured</CardHeader>
+      <CardBody>
+        {lessons && <h4 className={classes.cardTitle}>{lessons.length} Lessons</h4>}
+
+        {/* lessons list */}
+        <List>
+          {lessons.map((lesson) => (
+            <ListItem>
+              <ListItemIcon>
+                <School className={classes.icon}/>
+              </ListItemIcon>
+              <ListItemText
+                primary={truncate(lesson.title, { length: 24 })}
+              />
+              {lesson.video && true && lesson.free_preview && (
+                <span
+                  role="button"
+                  className="text-primary"
+                  onClick={() => {
+                    setPreview(lesson.video.Location)
+                    setShowModal(!showModal)
+                  }}
+                >
+                    Preview
+                  </span>
+              )}
+            </ListItem>
+          ))}
+        </List>
+
+      </CardBody>
+      <CardFooter className={classes.textMuted}>
+        {course.updatedAt && `Last Update: ${Moment(course.updatedAt).format('LL')}`}
+      </CardFooter>
+    </Card>
+  </>)
+}
+
+export default SingleCourseLessons
