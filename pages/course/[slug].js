bd00f4591a0b2a3aa2928739d5ee81740134a764
Joseph Dubon <josephdubon@pm.me>
2022-09-19 13:44:12 -0700

    cleaned up imports

--- pages/course/[slug].js
+++ pages/course/[slug].js
@@ -15,10 +15,10 @@ import ReactPlayer from 'react-player'
 import Footer from '../../components/Footer/Footer'
 import { makeStyles } from '@material-ui/core/styles'
 import styles from '../../styles/jss/americoders/pages/landingPage'
-import { SafetyCheck, HourglassBottom } from '@material-ui/icons'
+import CoursesFeatureSection from '../../pages-sections/CoursesPage-Sections/CoursesFeatureSection'
+import { HourglassBottom, SafetyCheck } from '@material-ui/icons'
 import { Tooltip } from '@mui/material'
 import Button from '../../components/CustomButtons/Button'
-import CoursesFeatureSection from '../../pages-sections/CoursesPage-Sections/CoursesFeatureSection'
 
 const Stripe = require('stripe')
 
