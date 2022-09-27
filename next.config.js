6342b8f34f4b9288c3f4bfbaf140d1a7c3e7e840
Joseph Dubon <josephdubon@pm.me>
2022-09-27 16:55:14 -0700

    Proxy the Analytics Script To avoid being blocked by adblockers plausible recommends proxying the script.
    Wrapped next.config.js

--- next.config.js
+++ next.config.js
@@ -1,5 +1,7 @@
-module.exports = {
+const { withPlausibleProxy } = require('next-plausible')
+
+module.exports = withPlausibleProxy()({
   images: {
     domains: ['americodersbucket.s3.us-west-2.amazonaws.com']
   }
-}
+})
