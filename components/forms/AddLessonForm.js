c34acaa16f01bd1f77106318c76e6a6051efd0bb
Joseph Dubon <josephdubon@pm.me>
2022-09-27 13:03:50 -0700

    fixed the formatting from emacs removed double quotes removed semicolons

--- components/forms/AddLessonForm.js
+++ components/forms/AddLessonForm.js
@@ -1,72 +1,75 @@
-import {Button, Progress, Tooltip} from 'antd'
-import {CloseCircleFilled} from '@ant-design/icons'
+import { Button, Progress, Tooltip } from 'antd'
+import { CloseCircleFilled } from '@ant-design/icons'
+import { Switch } from '@mui/material'
 
 const AddLessonForm = ({
-                           values,
-                           setValues,
-                           handleAddLesson,
-                           handleVideo,
-                           handleRemoveVideo,
-                           progress,
-                           uploading,
-                           uploadButtonText
-                       }) => {
-    return (<>
-        <div className='container pt-3'>
+  values,
+  setValues,
+  handleAddLesson,
+  handleVideo,
+  handleRemoveVideo,
+  progress,
+  uploading,
+  uploadButtonText
+}) => {
+  return (<>
+    <div className="container pt-3">
 
-            <form onSubmit={handleAddLesson}>
-                {/* title */}
-                <input
-                    type='text'
-                    className='form-control'
-                    onChange={(e) => setValues({...values, title: e.target.value})}
-                    value={values.title}
-                    placeholder='Title'
-                    autoFocus
-                    required
-                />
+      <form onSubmit={handleAddLesson}>
+        {/* title */}
+        <input
+          type="text"
+          className="form-control"
+          onChange={(e) => setValues({ ...values, title: e.target.value })}
+          value={values.title}
+          placeholder="Title"
+          autoFocus
+          required
+        />
 
-                {/* content */}
-                <textarea
-                    className='form-control mt-3'
-                    cols='7'
-                    rows='14'
-                    onChange={(e) => setValues({...values, content: e.target.value})}
-                    value={values.content}
-                    placeholder='Content'>
+        {/* content */}
+        <textarea
+          className="form-control mt-3"
+          cols="7"
+          rows="14"
+          onChange={(e) => setValues({ ...values, content: e.target.value })}
+          value={values.content}
+          placeholder="Content">
                 </textarea>
 
 
-                {/* code area */}
-                <div className='row form-group gap-2 mt-3 mb-3'>
-                    <textarea
-                        className='form-control mt-3 col'
-                        cols='7'
-                        rows='14'
-                        onChange={(e) => setValues({...values, html: e.target.value})}
-                        value={values.html}
-                        placeholder='HTML'>
+        {/* code area */}
+        <div className="row form-group gap-2 mt-3 mb-3">
+          {/* html */}
+          <textarea
+            className="form-control mt-3 col"
+            cols="7"
+            rows="14"
+            onChange={(e) => setValues({ ...values, html: e.target.value })}
+            value={values.html}
+            placeholder="HTML">
                     </textarea>
 
-                    <textarea
-                        className='form-control mt-3 col'
-                        cols='7'
-                        rows='14'
-                        onChange={(e) => setValues({...values, css: e.target.value})}
-                        value={values.css}
-                        placeholder='CSS'>
-
+          {/* css */}
+          <textarea
+            className="form-control mt-3 col"
+            cols="7"
+            rows="14"
+            onChange={(e) => setValues({ ...values, css: e.target.value })}
+            value={values.css}
+            placeholder="CSS">
                     </textarea>
 
-                    <textarea
-                        className='form-control mt-3 col'
-                        cols='7'
-                        rows='14'
-                        onChange={(e) => setValues({...values, javascript: e.target.value})}
-                        value={values.javascript}
-                        placeholder='JavaScript'>
+          {/* javascript */}
+          <textarea
+            className="form-control mt-3 col"
+            cols="7"
+            rows="14"
+            onChange={(e) => setValues({ ...values, javascript: e.target.value })}
+            value={values.javascript}
+            placeholder="JavaScript">
                     </textarea>
-                </div>
+        </div>
 
                 <div className='col d-grid gap-2'>
                     <label className='btn btn-dark btn-block text-left mt-3'>
@@ -79,37 +82,37 @@ const AddLessonForm = ({
                         />
                     </label>
 
-                    {!uploading && values.video.Location && (
-                        <Tooltip title='Remove'>
-                            <span onClick={handleRemoveVideo} className='pt-1 pl-3'>
-                                <CloseCircleFilled className='text-danger d-flex justify-content-center pt-4'/>
+          {!uploading && values.video.Location && (
+            <Tooltip title="Remove">
+                            <span onClick={handleRemoveVideo} className="pt-1 pl-3">
+                                <CloseCircleFilled className="text-danger d-flex justify-content-center pt-4"/>
                             </span>
-                        </Tooltip>
-                    )}
+            </Tooltip>
+          )}
 
-                    {progress > 0 && (
-                        <Progress
-                            className='d-flex justify-content-center pt-2'
-                            percent={progress}
-                            steps={10}
-                        />
-                    )}
+          {progress > 0 && (
+            <Progress
+              className="d-flex justify-content-center pt-2"
+              percent={progress}
+              steps={10}
+            />
+          )}
 
-                    <Button
-                        onClick={handleAddLesson}
-                        className='col mt-3'
-                        size='large'
-                        type='primary'
-                        shape='round'
-                        loading={uploading}
-                    >
-                        Save
-                    </Button>
-                </div>
-
-            </form>
+          <Button
+            onClick={handleAddLesson}
+            className="col mt-3"
+            size="large"
+            type="primary"
+            shape="round"
+            loading={uploading}
+          >
+            Save
+          </Button>
         </div>
-    </>)
+
+      </form>
+    </div>
+  </>)
 }
 
 export default AddLessonForm
