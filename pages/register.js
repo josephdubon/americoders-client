5a6e7cf36af0e09e0852f488bb89070a547faabe
Joseph Dubon <josephdubon@pm.me>
2022-09-30 00:17:54 -0700

    created new register page with new design and mui

--- pages/register.js
+++ pages/register.js
@@ -13,6 +13,7 @@ import styles from '../styles/jss/americoders/pages/loginPage.js'
 import StudentRegisterForm from '../components/forms/StudentRegisterForm'
 import Footer from '../components/Footer/Footer'
 import NavLogo from '../public/images/logo/americoders-logo-simple_white.svg'
+import { PageHead } from '../components/head/PageHead'
 
 const useStyles = makeStyles(styles)
 
@@ -23,45 +24,45 @@ export default function RegisterPage (props) {
   }, 700)
   const classes = useStyles()
   const { ...rest } = props
-  return (
-    <div>
-      <Header
-        absolute
-        color="transparent"
-        brand={NavLogo}
-        rightLinks={<HeaderLinks/>}
-        {...rest}
-      />
+  return (<>
+    <PageHead title={'New Student Registration'}/>
 
-      <div
-        className={classes.pageHeader}
-        style={{
-          backgroundImage: 'url(\'/images/original/americoders-community-hands-friends.jpg\')',
-          backgroundSize: 'cover',
-          backgroundPosition: 'top center',
-        }}
-      >
-        <div className={classes.container}>
-          <GridContainer
-            direction="row"
-            justifyContent="center"
-            alignItems="center"
+    <Header
+      absolute
+      color="transparent"
+      brand={NavLogo}
+      rightLinks={<HeaderLinks/>}
+      {...rest}
+    />
+
+    <div
+      className={classes.pageHeader}
+      style={{
+        backgroundImage: 'url(\'/images/original/americoders-community-hands-friends.jpg\')',
+        backgroundSize: 'cover',
+        backgroundPosition: 'top center',
+      }}
+    >
+      <div className={classes.container}>
+        <GridContainer
+          direction="row"
+          justifyContent="center"
+          alignItems="center"
+        >
+          <GridContainer direction="row"
+                         justifyContent="center"
+                         alignItems="center"
           >
-            <GridContainer direction="row"
-                           justifyContent="center"
-                           alignItems="center"
-            >
-              <GridItem xs={10} sm={6} md={6}>
-                <Card className={classes[cardAnimaton]}>
-                  {/* register form */}
-                  <StudentRegisterForm/>
-                </Card>
-              </GridItem>
-            </GridContainer>
+            <GridItem xs={10} sm={6} md={6}>
+              <Card className={classes[cardAnimaton]}>
+                {/* register form */}
+                <StudentRegisterForm/>
+              </Card>
+            </GridItem>
           </GridContainer>
-        </div>
-        <Footer whiteFont/>
+        </GridContainer>
       </div>
+      <Footer whiteFont/>
     </div>
-  )
+  </>)
 }
