617c51e0e03cb0bb9294116e233cf39b7c4d2efc
Joseph Dubon <josephdubon@pm.me>
2022-09-30 11:41:46 -0700

    removed unused imports

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -9,14 +9,12 @@ import { makeStyles } from '@material-ui/core/styles'
 import AppBar from '@material-ui/core/AppBar'
 import Toolbar from '@material-ui/core/Toolbar'
 import IconButton from '@material-ui/core/IconButton'
-import Button from '@material-ui/core/Button'
 import Hidden from '@material-ui/core/Hidden'
 import Drawer from '@material-ui/core/Drawer'
 // @material-ui/icons
 import Menu from '@material-ui/icons/Menu'
 // core components
 import styles from '../../styles/jss/americoders/components/headerStyle.js'
-import Image from 'next/image'
 
 const useStyles = makeStyles(styles)
 
