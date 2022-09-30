ba8db183e531058d44e43d8bff1d595eef9622da
Joseph Dubon <josephdubon@pm.me>
2022-09-30 11:43:18 -0700

    removed home tag

--- components/footer/Footer.js
+++ components/footer/Footer.js
@@ -49,6 +49,7 @@ export default function Footer (props) {
         <div className={classes.right}>
           &copy; {1900 + new Date().getYear()}
           {' '}
+          {/* link back home */}
           <Link href={'/landing'}>
             <a className={aClasses}>
               AMERICODERS
