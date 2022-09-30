6cd0be1a54a13162333d02596c275368e79ce4be
Joseph Dubon <josephdubon@pm.me>
2022-09-29 18:04:31 -0700

    redesigned instructor page to match rest of website using material ui

--- pages/instructor/index.js
+++ pages/instructor/index.js
@@ -46,80 +46,188 @@ const InstructorIndex = (props) => {
     fontSize: '10px',
   }
 
-  return (<InstructorRoute>
+  const classes = useStyles()
+  const { ...rest } = props
+  const imageClasses = classNames(
+    classes.imgRaised,
+    classes.imgFluid,
+  )
 
-    <PageHead title={'Instructor Dashboard'}/>
+  return (<>
+    {/* meta data */}
+    <PageHead title={`Welcome back, ${user && user.firstName} ${user &&
+    user.lastName}!`}/>
 
-    <main>
-      <section className="py-5 text-center container">
-        <div className="row py-lg-5">
-          <div className="col-lg-6 col-md-8 mx-auto">
-            <h1 className="fw-light">Instructor Dashboard</h1>
-          </div>
-        </div>
-      </section>
+    {/* navigation */}
+    <Header
+      color="transparent"
+      brand={NavLogo}
+      rightLinks={<HeaderLinks/>}
+      fixed
+      changeColorOnScroll={{
+        height: 200,
+        color: 'white',
+      }}
+      {...rest}
+      sx={{ zIndex: (theme) => theme.zIndex.drawer + 1 }}
+    />
 
-      <div className="album py-5 bg-light">
-        <div className="container">
-          <div className="row row-cols-1 row-cols-sm-1 row-cols-md-1 g-1">
-            {/* list all courses */}
-            {courses && courses.map(course => (<div key={course._id}>
-              {/* parent media div */}
-              <div className="d-flex align-items-center pt-2">
-                {/* image media div */}
-                <div className="flex-shrink-0">
-                  {/* image source */}
-                  <Avatar
-                    size={80}
-                    src={course.image ? course.image.Location : '/images/americoders-course.png'}
-                  />
+    {/* hero section */}
+    <Parallax small filter
+              image="/images/americoders-community-diversity.png"/>
+
+    {/* main content */}
+    <div className={classNames(classes.main, classes.mainRaised)}>
+      <div>
+        <div className={classes.container}>
+          {/* flex container */}
+          <GridContainer
+            direction="column"
+            justifyContent="center"
+            alignItems="center"
+          >
+            {/* flex container - sub */}
+            <GridContainer direction="row"
+                           justifyContent="center"
+                           alignItems="center"
+                           spacing={2}>
+              {/* intro */}
+              <GridItem xs={12} sm={12} md={12}>
+                <div className={classes.profile}>
+                  <div>
+                    <img
+                      src={user && user.picture}
+                      alt="..."
+                      className={imageClasses}
+                      style={{ marginBottom: '25px' }}
+                    />
+                  </div>
+                  <div className={classes.name}>
+                    {/* name */}
+                    <h3 className={classes.title}>
+                      Instructor Dashboard
+                    </h3>
+                    <br/>
+                    <h5> Welcome back, Instructor {user && user.lastName}!</h5>
+                    {/* user details */}
+                    <h6>Courses Listed: {courses && courses.length + ' '}</h6>
+                    <h6>Member Since: {Moment(user && user.createdAt,
+                      'YYYYMMDD').fromNow()}</h6>
+                  </div>
                 </div>
-                {/* media text body */}
-                <div className="flex-grow-1 ms-3">
-                  {/* title / link to course*/}
-                  <Link
-                    href={`/instructor/course/view/${course.slug}`}
-                  >
-                    <a className="mt-2 text-primary"><h5 className="pt-2">{course.name}</h5></a>
-                  </Link>
-                  <p>{
-                    // show number of lessons in course
-                    course.lessons.length} Lessons</p>
-                  {
-                    // show requirements message
-                    course.lessons.length < 5 ? (
-                        <p style={myStyle} className="text-warning">At least 5 lessons are required
-                          to publish a course.</p>
-                      ) :
-                      // show success message
-                      course.published ? (
-                          <p style={myStyle} className="text-success">Your course is live in the
-                            marketplace.</p>
-                        ) :
-                        // show 'ready to publish' message
-                        (
-                          <p style={myStyle} className="text-success">Your course is ready to
-                            be published.</p>
-                        )}
+              </GridItem>
+              <GridItem>
+                <div className={classes.description}>
+                  <p>
+                    {user && user.bio}
+                  </p>
                 </div>
-                <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
-                  {course.published ? (
-                    <Tooltip title="Published">
-                      <CheckCircleOutlined className="h5 text-success"/>
-                    </Tooltip>
-                  ) : (
-                    <Tooltip title="Unpublished">
-                      <CloseCircleOutlined className="h5 text-warning"/>
-                    </Tooltip>
-                  )}
-                </div>
-              </div>
-            </div>))}
-          </div>
+              </GridItem>
+            </GridContainer>
+
+            {/* courses container */}
+            <GridContainer direction="row"
+                           justifyContent="center"
+                           alignItems="center"
+                           spacing={2}>
+              <GridItem xs={12} sm={12} md={12}
+                        className={classes.navWrapper}>
+                <NavPills
+                  alignCenter
+                  color="primary"
+                  tabs={[
+                    {
+                      tabButton: 'My Courses',
+                      tabIcon: School,
+                      tabContent: (
+                        <GridContainer
+                          direction="row"
+                          justifyContent="center"
+                          alignItems="center"
+                        >
+                          <GridContainer direction="row"
+                                         justifyContent="center"
+                                         alignItems="center"
+                          >
+
+                            {/* list all courses */}
+                            {courses &&
+                              courses.map(course => (<div key={course._id}>
+                                {/* parent media div */}
+                                <GridContainer
+                                  justifyContent="center"
+                                  alignItems="center"
+                                >
+                                  {/* image media div */}
+                                  <GridItem xs={11} sm={11} md={2}>
+                                    {/* image source */}
+                                    <Avatar
+                                      size={80}
+                                      src={course.image
+                                        ? course.image.Location
+                                        : '/images/americoders-course.png'}
+                                    />
+                                  </GridItem>
+
+                                  {/* media text body */}
+                                  <GridItem xs={11} sm={11} md={8}>
+                                    {/* title / link to course*/}
+                                    <Link
+                                      href={`/instructor/course/view/${course.slug}`}
+                                    >
+                                      <a><h5>{course.name}</h5></a>
+                                    </Link>
+                                    <p>{
+                                      // show number of lessons in course
+                                      course.lessons.length} Lessons</p>
+                                    {
+                                      // show requirements message
+                                      course.lessons.length < 5 ? (
+                                          <p style={myStyle}
+                                             className="text-warning">At least 5 lessons are required to publish a
+                                                                      course.</p>
+                                        ) :
+                                        // show success message
+                                        course.published ? (
+                                            <p style={myStyle}
+                                               className="text-success">Your course is live in the marketplace.</p>
+                                          ) :
+                                          // show 'ready to publish' message
+                                          (
+                                            <p style={myStyle}
+                                               className="text-success">Your course is ready to be published.</p>
+                                          )}
+                                  </GridItem>
+
+                                  <GridItem xs={11} sm={11} md={2}>
+                                    {course.published ? (
+                                      <Tooltip title="Published">
+                                        <CheckCircleOutlined
+                                          className="h5 text-success"/>
+                                      </Tooltip>
+                                    ) : (
+                                      <Tooltip title="Unpublished">
+                                        <CloseCircleOutlined
+                                          className="h5 text-warning"/>
+                                      </Tooltip>
+                                    )}
+                                  </GridItem>
+                                </GridContainer>
+                              </div>))}
+                          </GridContainer>
+                        </GridContainer>
+                      ),
+                    },
+                  ]}
+                />
+              </GridItem>
+            </GridContainer>
+          </GridContainer>
         </div>
       </div>
-    </main>
-  </InstructorRoute>)
+    </div>
+    <Footer/>
+  </>)
 }
 
 export default InstructorIndex
