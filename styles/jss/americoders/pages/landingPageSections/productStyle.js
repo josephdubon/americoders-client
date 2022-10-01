31f86b9a3013c9c33c6d6bdcea1960c396987a0d
Joseph Dubon <josephdubon@pm.me>
2022-10-01 15:05:34 -0700

    added markdown file styles linked to main file css markdown style

--- styles/jss/americoders/pages/landingPageSections/productStyle.js
+++ styles/jss/americoders/pages/landingPageSections/productStyle.js
@@ -1,4 +1,4 @@
-import { title } from '../../../americoders'
+import { markdownStyles, title } from '../../../americoders'
 
 const productStyle = {
   section: {
@@ -24,13 +24,15 @@ const productStyle = {
     textAlign: 'center',
     margin: '.5rem 1rem',
   },
+  markdown: {
+    ...markdownStyles,
+  },
   quote: {
     color: '#4b4b4b',
     fontSize: '.8rem',
     fontWeight: '400',
     textAlign: 'center',
   },
-
   image: {
     borderRadius: '2rem',
   },
