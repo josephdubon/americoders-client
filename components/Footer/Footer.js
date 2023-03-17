89b51dbe59fd7f5c4f6668e5f25a82c4764b2e68
Joseph Dubon <josephdubon@pm.me>
2023-03-17 14:04:11 -0700

    added formatting

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -15,6 +15,7 @@ import { ListItem } from '@mui/material'
 
 const useStyles = makeStyles(styles)
 
+export default function Footer(props) {
   const classes = useStyles()
   const { whiteFont } = props
   const footerClasses = classNames({
@@ -33,6 +34,7 @@ const useStyles = makeStyles(styles)
             {/* add left side links here */}
             <ListItem className={classes.inlineBlock}>
               <Link href={'/landing'} as={'/home'}>
+                <a className={classes.block}>Home</a>
               </Link>
             </ListItem>
           </List>
@@ -40,6 +42,7 @@ const useStyles = makeStyles(styles)
 
         {/* right side credit line */}
         <div className={classes.right}>
+          &copy; {1900 + new Date().getYear()}{' '}
           <Link href={'/landing'} as={'/home'}>
             <a className={aClasses}>AMERICODERS</a>
           </Link>
